947543d49b11f06cd77cf44c0c0de81f3e86bef6
Alex Harker <ajharker@gmail.com>
2017-07-08 12:19:51 +0100

    Updated numeric tests

--- NumericTesting/main.cpp
+++ NumericTesting/main.cpp
@@ -77,17 +77,19 @@ bool equal(FL_FP a, double b)
     return false;
 }
 
+bool roundCheck(uint64_t a)                        { return (a & 0x8000000000000000ULL) ? true : false; }
+
 int main(int argc, const char * argv[]) {
     
     //FL_FP num = FL_FP(5, 35798);//(9.34567434);
     //FL_FP num = FL_FP(9, 0x587e1d115dcb894c);//(9.34567434);
     //FL_FP denom = FL_FP(3547938458, 0x00000000FEFEFEF1);
-    FL_FP denom = FL_FP(0, 1);
+    FL_FP denom = FL_FP(0U, 1U);
 
     //FL_FP num = FL_FP(7892343, 4378529);//(9.34567434);
    // FL_FP denom = FL_FP(9, 0xF0000000FEFEFEF1);
     
-    FL_FP num = FL_FP(0, 87430);//(9.34567434);
+    FL_FP num = FL_FP(0U, 87430U);//(9.34567434);
 
     // Multiplication commutation test
     
@@ -136,7 +138,7 @@ int main(int argc, const char * argv[]) {
     for (uint64_t i=0; i <= 0xFFFFFF; i++)
     {
         FL_SP rand1 = randSP();
-        FL_SP rand2 = FL_SP(randu64(), randu64(), 0);
+        FL_SP rand2 = FL_SP(randu64(), randu64(), 0U);
         
         FL_SP result1 = rand1 * rand2;
         FL_SP result2 = qMul(rand1, rand2.intVal(), rand2.fracHiVal());
@@ -146,11 +148,11 @@ int main(int argc, const char * argv[]) {
     }
     
     // SP and Fixed comparison
-    /*
+    
     for (uint64_t i=0; i <= 0xFFFFFF; i++)
     {
         FL_SP rand1 = randSP();
-        FL_SP rand2 = FL_SP(randu64(), randu64(), 0);
+        FL_SP rand2 = FL_SP(randu64(), randu64(), 0U);
         
         FL_SP result1 = rand1 * rand2;
         FL_SP result2 = qMul(rand1, rand2.intVal(), rand2.fracHiVal());
@@ -167,13 +169,16 @@ int main(int argc, const char * argv[]) {
         FL_FP rand2 = randFixed();
         
         FL_FP result1 = rand1 * rand2;
-        FL_SP result2 = qMul(rand1, rand2.intVal(), rand2.fracHiVal());
+        FL_SP result2 = qMul(FL_SP(rand1.intVal(), rand1.fracVal(), 0U), rand2.intVal(), rand2.fracVal());
         
-        if ((result1.intVal() != result2.intVal()) || (result1.fracHiVal() != result2.fracHiVal()) || (result2.fracLoVal() != result2.fracLoVal()))
+        if ((result1.intVal() != result2.intVal()) || ((result1.fracVal() != result2.fracHiVal()) && !(roundCheck(result2.fracLoVal()) && (result1.fracVal() - FUInt64(1U)) == result2.fracHiVal())))
             std::cout << "fixed mul not equal to qMul " << i << "\n";
-    }*/
+    }
     
     // Simple Divide Check
+    
+    uint64_t correct = 0;
+    uint64_t between = 0 ;
     
     for (uint64_t i=0; i <= 0xFFFFFF; i++)
     {
@@ -182,19 +187,28 @@ int main(int argc, const char * argv[]) {
         FL_FP result = a / b;
         
         FL_FP check1 = result * b;
-        FL_FP check2 = (result + FL_FP(0,1)) * b;
-        FL_FP check3 = (result - FL_FP(0,1)) * b;
+        FL_FP check2 = (result + FL_FP(0U,1U)) * b;
+        FL_FP check3 = (result - FL_FP(0U,1U)) * b;
         
         if (check1 == a)
+        {
+            correct++;
             continue;
+        }
         
         if (check2 > a && check3 < a)
+        {
+            between++;
             continue;
+        }
         
-        std::cout << "failed \n";
+        std::cout << "failed\n";
+
         break;
     }
 
+    std::cout << "correct " << correct << "\n";
+    std::cout << "between " << between << "\n";
     std::cout << "done simple\n";
     
     // Hard Divide Check
@@ -204,14 +218,14 @@ int main(int argc, const char * argv[]) {
         FL_FP a = randSmallFixed();
         FL_FP b = randSmallFixed();
         FL_FP result = a / b;
-        FL_FP resultP1 = result + FL_FP(0,1);
-        FL_FP resultM1 = result - FL_FP(0,1);
+        FL_FP resultP1 = result + FL_FP(0U,1U);
+        FL_FP resultM1 = result - FL_FP(0U,1U);
 
-        FL_SP a1(a.intVal(), a.fracVal(), 0);
-        FL_SP b1(b.intVal(), b.fracVal(), 0);
-        FL_SP r1(result.intVal(), result.fracVal(), 0);
-        FL_SP rP1(resultP1.intVal(), resultP1.fracVal(), 0);
-        FL_SP rM1(resultM1.intVal(), resultM1.fracVal(), 0);
+        FL_SP a1(a.intVal(), a.fracVal(), 0U);
+        FL_SP b1(b.intVal(), b.fracVal(), 0U);
+        FL_SP r1(result.intVal(), result.fracVal(), 0U);
+        FL_SP rP1(resultP1.intVal(), resultP1.fracVal(), 0U);
+        FL_SP rM1(resultM1.intVal(), resultM1.fracVal(), 0U);
         
         FL_SP check1 = diff(a1, r1 * b1);
         FL_SP check2 = diff(a1, rP1 * b1);
@@ -259,9 +273,9 @@ int main(int argc, const char * argv[]) {
         FL_FP result = FL_FP(1.0);
     
         for (uint64_t i=0; i <= 0xFFFFFFF; i++)
-            result *= FL_FP(2,0x8000000000000000ULL);
+            result *= FL_FP(2U,0x8000000000000000ULL);
         
-            if (result == FL_FP(0,0))
+            if (result == FL_FP(0U,0U))
                 std::cout << "equals zero\n";
     }
 
@@ -297,9 +311,9 @@ int main(int argc, const char * argv[]) {
 
     for (uint64_t i=0; i <= 0x8FFFFFF; i++)
     {
-        FL_FP result = FL_FP(0,i) / FL_FP(0,1);
+        FL_FP result = FL_FP(0U,i) / FL_FP(0U,1U);
         
-        if (result != FL_FP(i,0))
+        if (result != FL_FP(i,0U))
             std::cout << "failed " << i << "\n";
     }
     
