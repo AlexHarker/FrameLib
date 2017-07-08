29500ffbb0524a154b5cfd54b7108bebca736211
Alex Harker <ajharker@gmail.com>
2017-07-08 20:40:44 +0100

    Many Test Improvements

--- NumericTesting/main.cpp
+++ NumericTesting/main.cpp
@@ -1,48 +1,21 @@
 
 #include <mach/mach.h>
 #include <mach/mach_time.h>
+#include <sstream>
 #include <iostream>
 #include <iomanip>
 #include "FrameLib_FixedPoint.h"
 #include "FrameLib_RandGen.h"
 
-class Timer
+void tabbedOut(const std::string& name, const std::string& text, int tab = 25)
 {
-    
-public:
-    
-    Timer() : mStart(0), mStore1(0), mStore2(0) {}
-    
-    void start()
-    {
-        mStart = mach_absolute_time();
-    };
-    
-    void stop(const std::string& msg)
-    {
-        uint64_t end = mach_absolute_time();
-        
-        mach_timebase_info_data_t info;
-        mach_timebase_info(&info);
-        
-        uint64_t elapsed = ((end - mStart) * info.numer) / info.denom;
-        std::cout << msg << " elapsed " << (elapsed / 1000000.0) << "\n";
-        
-        mStore2 = mStore1;
-        mStore1 = elapsed;
-    };
-    
-    void relative(const std::string& msg)
-    {
-        std::cout << msg << " relative " << ((double) mStore1 / (double) mStore2) << "\n";
-    }
-
-private:
-    
-    uint64_t        mStart;
-    uint64_t        mStore1;
-    uint64_t        mStore2;
-};
+    std::cout << std::setw(tab) << std::setfill(' ');
+    std::cout.setf(std::ios::left);
+    std::cout.unsetf(std::ios::right);
+    std::cout << name;
+    std::cout.unsetf(std::ios::left);
+    std::cout << text << "\n";
+}
 
 void numberOut(const std::string& name, FL_FP in)
 {
@@ -188,30 +161,121 @@ Equality equalCheck(FL_FP a, double b)
 
 bool roundCheck(uint64_t a)                        { return (a & 0x8000000000000000ULL) ? true : false; }
 
-void postPercentage(std::string msg, uint64_t count, uint64_t total)
+template <typename T>
+std::string to_string_with_precision(const T a_value, const int n = 4, bool fixed = true)
+{
+    std::ostringstream out;
+    if (fixed)
+        out << std::setprecision(n) << std::fixed << a_value;
+    else
+        out << std::setprecision(n) << a_value;
+
+    return out.str();
+}
+
+std::string getPercentage(uint64_t count, uint64_t total)
 {
     double percentage = 100.0 * ((double) count / (double) total);
     
-    std::cout << msg << " " << percentage << "%\n";
+    std::string outStr(to_string_with_precision(percentage, 4, false) + "%");
+    
+    return outStr;
 }
 
-int main(int argc, const char * argv[]) {
+void postPercentage(std::string msg, uint64_t count, uint64_t total)
+{
+    std::cout << msg << " " << getPercentage(count, total) << "\n";
+}
+
+// ************************************************************************************** //
+
+// Correctness
+
+typedef uint64_t testFunc(uint64_t count);
+
+void runTest(std::string name, testFunc func, uint64_t count)
+{
+    std::cout << name << " - Test Started\n";
+
+    uint64_t done = func(count);
+
+    if (done == count)
+        std::cout << name << " - Test Passed\n";
+    else
+        postPercentage(name + " : - TEST FAILED AT", done, count);
     
+    std::cout << "\n";
+}
+
+// ************************************************************************************** //
+
+// Timing
+
+class Timer
+{
+    
+public:
+    
+    Timer() : mStart(0), mStore1(0), mStore2(0) {}
+    
+    void start()
+    {
+        mStart = mach_absolute_time();
+    };
+    
+    void stop(const std::string& msg)
+    {
+        uint64_t end = mach_absolute_time();
+        
+        mach_timebase_info_data_t info;
+        mach_timebase_info(&info);
+        
+        uint64_t elapsed = ((end - mStart) * info.numer) / info.denom;
+        tabbedOut(msg + " Elapsed ", to_string_with_precision(elapsed / 1000000.0, 2), 35);
+        
+        mStore2 = mStore1;
+        mStore1 = elapsed;
+    };
+    
+    void relative(const std::string& msg)
+    {
+        tabbedOut(msg + " Comparison ", to_string_with_precision(((double) mStore1 / (double) mStore2), 2), 35);
+    }
+    
+private:
+    
+    uint64_t        mStart;
+    uint64_t        mStore1;
+    uint64_t        mStore2;
+};
+
+typedef void timingCompareFunc(uint64_t count);
+
+void runTimeCompareTest(std::string name, std::string name1, std::string name2, timingCompareFunc func1, timingCompareFunc func2, uint64_t count)
+{
     Timer timeMeasure;
+    std::cout << name << " - Test Started\n";
     
-    //FL_FP num = FL_FP(5, 35798);//(9.34567434);
-    //FL_FP num = FL_FP(9, 0x587e1d115dcb894c);//(9.34567434);
-    //FL_FP denom = FL_FP(3547938458, 0x00000000FEFEFEF1);
-    FL_FP denom = FL_FP(0U, 1U);
-
-    //FL_FP num = FL_FP(7892343, 4378529);//(9.34567434);
-   // FL_FP denom = FL_FP(9, 0xF0000000FEFEFEF1);
+    timeMeasure.start();
+    func1(count);
+    timeMeasure.stop(name1);
+    func2(count);
+    timeMeasure.stop(name2);
+    timeMeasure.relative(name);
     
-    FL_FP num = FL_FP(0U, 87430U);//(9.34567434);
+    std::cout << name << " - Test Completed\n";
+    std::cout << "\n";
+}
 
+// ************************************************************************************** //
+
+// Correctness tests
+
+uint64_t multCommutationTest(uint64_t count)
+{
     // Multiplication commutation test
     
-    for (uint64_t i=0; i <= 0xFFFFFF; i++)
+    for (uint64_t i = 0; i < count; i++)
     {
         FL_FP rand1 = randFixed(i);
         FL_FP rand2 = randFixed(i);
@@ -219,9 +283,14 @@ int main(int argc, const char * argv[]) {
         FL_FP result2 = rand2 * rand1;
         
         if (result1 != result2)
-            std::cout << "Fixed mul not commutative " << i << "\n";
+            return i;
     }
     
+    return count;
+}
+
+uint64_t  multDoubleComparisonTest(uint64_t count)
+{
     // Multiplication double comparison test
     
     uint64_t exact = 0;
@@ -229,16 +298,15 @@ int main(int argc, const char * argv[]) {
     uint32_t nextULP = 0;
     uint32_t nextDouble = 0;
     uint32_t oneULP = 0;
-    uint32_t inaccurate = 0;
     
-    for (uint64_t i=0; i <= 0xFFFFFF; i++)
+    for (uint64_t i = 0; i < count; i++)
     {
         double rand1D = (double) randFixed(i);
         double rand2D = (double) randFixed(i);
         FL_FP rand1 = rand1D;
         FL_FP rand2 = rand2D;
         FL_FP result1 = (rand1 * rand2);
-
+        
         double result2 = rand2D * rand1D;
         
         switch (equalCheck(result1, result2))
@@ -248,23 +316,24 @@ int main(int argc, const char * argv[]) {
             case kNextDouble:       nextDouble++;       break;
             case kNextDoubleOneULP: nextULP++;          break;
             case kOneULP:           oneULP++;           break;
-            case kInaccurate:       inaccurate++;       break;
+            case kInaccurate:       return i;
         }
     }
     
-    if (inaccurate)
-        std::cout << "FAILED double multiplication comparison test\n";
+    tabbedOut("exact double", getPercentage(exact, 0xFFFFFF));
+    tabbedOut("equal fixed", getPercentage(equalFP, 0xFFFFFF));
+    tabbedOut("one double / ULP", getPercentage(nextULP, 0xFFFFFF));
+    tabbedOut("next double", getPercentage(nextDouble, 0xFFFFFF));
+    tabbedOut("one ULP out", getPercentage(oneULP, 0xFFFFFF));
 
-    postPercentage("exact", exact, 0xFFFFFF);
-    postPercentage("equal as fixed point", equalFP, 0xFFFFFF);
-    postPercentage("next double / one ULP", nextULP, 0xFFFFFF);
-    postPercentage("next double", nextDouble, 0xFFFFFF);
-    postPercentage("one ULP out", oneULP, 0xFFFFFF);
-    postPercentage("inaccurate", inaccurate, 0xFFFFFF);
+    return count;
+}
 
+uint64_t  multSPCommutationTest(uint64_t count)
+{
     // Multiplication SP commutation test
     
-    for (uint64_t i=0; i <= 0xFFFFFF; i++)
+    for (uint64_t i = 0; i < count; i++)
     {
         FL_SP rand1 = randSP();
         FL_SP rand2 = randSP();
@@ -272,26 +341,17 @@ int main(int argc, const char * argv[]) {
         FL_SP result2 = rand2 * rand1;
         
         if ((result1.intVal() != result2.intVal()) || (result1.fracHiVal() != result2.fracHiVal()) || (result1.fracLoVal() != result2.fracLoVal()))
-            std::cout << "SP mul not commutative " << i << "\n";
+            return i;
     }
     
-    // SP and QMul comparison
-    
-    for (uint64_t i=0; i <= 0xFFFFFF; i++)
-    {
-        FL_SP rand1 = randSP();
-        FL_SP rand2 = FL_SP(randu64(), randu64(), 0U);
-        
-        FL_SP result1 = rand1 * rand2;
-        FL_SP result2 = qMul(rand1, rand2.intVal(), rand2.fracHiVal());
-        
-        if ((result1.intVal() != result2.intVal()) || (result1.fracHiVal() != result2.fracHiVal()) || (result1.fracLoVal() != result2.fracLoVal()))
-            std::cout << "SP mul not equal to qMul " << i << "\n";
-    }
-    
+    return count;
+}
+
+uint64_t  multSPFixedComparisonTest(uint64_t count)
+{
     // SP and Fixed comparison
     
-    for (uint64_t i=0; i <= 0xFFFFFF; i++)
+    for (uint64_t i = 0; i < count; i++)
     {
         FL_SP rand1 = randSP();
         FL_SP rand2 = FL_SP(randu64(), randu64(), 0U);
@@ -300,12 +360,17 @@ int main(int argc, const char * argv[]) {
         FL_SP result2 = qMul(rand1, rand2.intVal(), rand2.fracHiVal());
         
         if ((result1.intVal() != result2.intVal()) || (result1.fracHiVal() != result2.fracHiVal()) || (result1.fracLoVal() != result2.fracLoVal()))
-            std::cout << "SP mul not equal to fixed " << i << "\n";
+            return i;
     }
     
+    return count;
+}
+
+uint64_t multQMulFixedComparisonTest(uint64_t count)
+{
     // QMul and Fixed comparison
     
-    for (uint64_t i=0; i <= 0xFFFFFF; i++)
+    for (uint64_t i = 0; i < count; i++)
     {
         FL_FP rand1 = randFixed(i);
         FL_FP rand2 = randFixed(i);
@@ -314,15 +379,37 @@ int main(int argc, const char * argv[]) {
         FL_SP result2 = qMul(FL_SP(rand1.intVal(), rand1.fracVal(), 0U), rand2.intVal(), rand2.fracVal());
         
         if ((result1.intVal() != result2.intVal()) || ((result1.fracVal() != result2.fracHiVal()) && !(roundCheck(result2.fracLoVal()) && (result1.fracVal() - 1) == result2.fracHiVal())))
-            std::cout << "fixed mul not equal to qMul " << i << "\n";
+            return i;
     }
     
-    // Simple Divide Check
+    return count;
+}
+
+uint64_t multSPQMulComparisonTest(uint64_t count)
+{
+    // SP and QMul comparison
     
-    uint64_t exact2 = 0;
+    for (uint64_t i = 0; i < count; i++)
+    {
+        FL_SP rand1 = randSP();
+        FL_SP rand2 = FL_SP(randu64(), randu64(), 0U);
+        
+        FL_SP result1 = rand1 * rand2;
+        FL_SP result2 = qMul(rand1, rand2.intVal(), rand2.fracHiVal());
+        
+        if ((result1.intVal() != result2.intVal()) || (result1.fracHiVal() != result2.fracHiVal()) || (result1.fracLoVal() != result2.fracLoVal()))
+            return i;
+    }
+    
+    return count;
+}
+
+uint64_t simpleDivideTest(uint64_t count)
+{
+    uint64_t exact = 0;
     uint64_t between = 0;
     
-    for (uint64_t i = 0; i <= 0xFFFFFF; i++)
+    for (uint64_t i = 0; i < count; i++)
     {
         FL_FP a = randSmallFixed();
         FL_FP b = randSmallFixed();
@@ -333,37 +420,31 @@ int main(int argc, const char * argv[]) {
         FL_FP check3 = (result - FL_FP(0U,1U)) * b;
         
         if (check1 == a)
-        {
-            exact2++;
-            continue;
-        }
-        
-        if (check2 > a && check3 < a)
-        {
+            exact++;
+        else if (check2 > a && check3 < a)
             between++;
-            continue;
-        }
-        
-        std::cout << "FAILED\n";
-
-        break;
+        else
+            return i;
     }
-
-    postPercentage("exact", exact2, 0xFFFFFF);
-    postPercentage("between", between, 0xFFFFFF);
-
-    std::cout << "done simple\n";
     
-    // Hard Divide Check
- 
-    for (uint64_t i=0; i <= 0xFFFFFF; i++)
+    tabbedOut("exact", getPercentage(exact, 0xFFFFFF));
+    tabbedOut("between", getPercentage(between, 0xFFFFFF));
+
+    return count;
+}
+
+uint64_t strictDivideTest(uint64_t count)
+{
+    // Strict Divide Check
+    
+    for (uint64_t i = 0; i < count; i++)
     {
         FL_FP a = randSmallFixed();
         FL_FP b = randSmallFixed();
         FL_FP result = a / b;
         FL_FP resultP1 = result + FL_FP(0U,1U);
         FL_FP resultM1 = result - FL_FP(0U,1U);
-
+        
         FL_SP a1(a.intVal(), a.fracVal(), 0U);
         FL_SP b1(b.intVal(), b.fracVal(), 0U);
         FL_SP r1(result.intVal(), result.fracVal(), 0U);
@@ -379,64 +460,83 @@ int main(int argc, const char * argv[]) {
         if (!(check3 < check1) && !(check2 < check1))
             continue;
         
-        std::cout << "FAILED \n";
-        break;
+        return i;
     }
     
-    std::cout << "done hard\n";
-    
-    timeMeasure.start();
-    
-    {
-        double result = 1.0;
+    return count;
+}
 
-        for (uint64_t i=0; i <= 0xFFFFFFF; i++)
-            result *= 2.5;
-        
-        if (result == 0.0)
-            std::cout << "equals zero\n";
-    }
+// ************************************************************************************** //
+
+// Speed tests
+
+void doubleMultiplySpeedTest(uint64_t count)
+{
+    double result = 1.0;
     
-    timeMeasure.stop("double multiply");
-    timeMeasure.start();
-
-    {
-        FL_FP result = FL_FP(1.0);
+    for (uint64_t i = 0; i < count; i++)
+        result *= 2.5;
     
-        for (uint64_t i=0; i <= 0xFFFFFFF; i++)
-            result *= FL_FP(2U,0x8000000000000000ULL);
-        
-            if (result.fracVal() == 0)
-                std::cout << "equals zero\n";
-    }
+    if (result == 0.0)
+        std::cout << "should not happen\n";
+}
 
-    timeMeasure.stop("FP_FP multiply");
-    timeMeasure.relative("FP_FP/double multiply");
-    timeMeasure.start();
+void fixedMultiplySpeedTest(uint64_t count)
+{
+    FL_FP result = FL_FP(1.0);
+    
+    for (uint64_t i = 0; i < count; i++)
+        result *= FL_FP(2U,0x8000000000000000ULL);
+    
+    if (result.fracVal() == 0)
+        std::cout << "should not happen\n";
+}
 
-    ///                      
-    for (uint64_t i=0; i <= 0x8FFFFFF; i++)
+void doubleDivideSpeedTest(uint64_t count)
+{
+    for (uint64_t i = 0; i < count; i++)
     {
         double result = 1.0 / (i + 2);
         
         if (result == i)
-            std::cout << "FAILED " << i << "\n";
+            std::cout << "should not happen\n";
     }
-    
-    timeMeasure.stop("double divide");
-    timeMeasure.start();
+}
 
-    for (uint64_t i=0; i <= 0x8FFFFFF; i++)
+void fixedDivideSpeedTest(uint64_t count)
+{
+    for (uint64_t i = 0; i < count; i++)
     {
         FL_FP result = FL_FP(0U,i) / FL_FP(0U,1U);
         
         if (result != FL_FP(i,0U))
-            std::cout << "FAILED " << i << "\n";
+            std::cout << "should not happen\n";
     }
+}
+
+// ************************************************************************************** //
+
+
+int main(int argc, const char * argv[]) {
     
-    timeMeasure.stop("FP_FP divide");
-    timeMeasure.relative("FP_FP/double divide");
-    timeMeasure.start();
+    std::cout << "CORRECTNESS TESTS\n\n";
+    
+    runTest("Multiplication Commutation", &multCommutationTest, 0xFFFFFF);
+    runTest("Multiplication SP Commutation", &multSPCommutationTest, 0xFFFFFF);
+    runTest("Multiplication Double Comparison", &multDoubleComparisonTest, 0xFFFFFF);
+    runTest("Multiplication SP Comparison", &multSPFixedComparisonTest, 0xFFFFFF);
+    runTest("Multiplication QMul Comparison", &multQMulFixedComparisonTest, 0xFFFFFF);
+    runTest("Multiplication SP QMul Comparison", &multSPQMulComparisonTest, 0xFFFFFF);
+    runTest("Simple Divide", &simpleDivideTest, 0xFFFFFF);
+    runTest("Strict Divide", &strictDivideTest, 0xFFFFFF);
+    
+    std::cout << "TIMING TESTS\n\n";
+    
+    runTimeCompareTest("Multiply Speed", "Double", "Fixed", &doubleMultiplySpeedTest, &fixedMultiplySpeedTest, 0xFFFFFFF);
+    runTimeCompareTest("Divide Speed", "Double", "Fixed", &doubleDivideSpeedTest, &fixedDivideSpeedTest, 0x8FFFFFF);
+    
+    FL_FP num = FL_FP(0U, 87430U);
+    FL_FP denom = FL_FP(0U, 1U);
     
     FL_FP divtemp = (num / denom);
     
