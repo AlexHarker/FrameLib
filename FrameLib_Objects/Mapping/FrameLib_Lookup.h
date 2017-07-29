fd82ffd2df3009c47434cf49a5df8838dece8206
Alex Harker <ajharker@gmail.com>
2017-07-29 11:13:09 +0100

    Split mapping objects into header / cpp

--- FrameLib_Objects/Mapping/FrameLib_Lookup.h
+++ FrameLib_Objects/Mapping/FrameLib_Lookup.h
@@ -9,36 +9,15 @@ class FrameLib_Lookup : public FrameLib_Processor
     
 public:
     
-    FrameLib_Lookup (FrameLib_Context context, FrameLib_Parameters::Serial *serialisedParameters, void *owner) : FrameLib_Processor(context, 2, 1)
-    {
-        // FIX - loads of different mode options here (mapping of positions + padding values
-        
-        mParameters.set(serialisedParameters);
-        
-        inputMode(1, false, false, false);
-    }
+    // Constructor
+
+    FrameLib_Lookup(FrameLib_Context context, FrameLib_Parameters::Serial *serialisedParameters, void *owner);
     
 private:
     
-    void process()
-    {
-        unsigned long sizeIn1, sizeIn2, sizeOut;
-        
-        double *input1 = getInput(0, &sizeIn1);
-        double *input2 = getInput(1, &sizeIn2);
-        
-        requestOutputSize(0, sizeIn1);
-        allocateOutputs();
-        
-        double *output = getOutput(0, &sizeOut);
-        
-        for (unsigned long i = 0; i < sizeOut; i++)
-        {
-            long pos = input1[i];
-            
-            output[i] = (pos >= 0) && (pos < sizeIn2) ? input2[pos] : 0.0;
-        }
-    }
+    // Process
+    
+    void process();
 };
 
 #endif
