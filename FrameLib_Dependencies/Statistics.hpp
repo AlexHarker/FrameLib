455eaea489946e8cccf32ef05568b7eb9c8295dc
Alex Harker <ajharker@gmail.com>
2017-08-30 16:28:33 +0100

    Updates to stats, opening up more object possibilities

---
+++ FrameLib_Dependencies/Statistics.hpp
@@ -0,0 +1,276 @@
+
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+// Helper functors
+
+struct Pass      { template <class T> T operator()(T a) { return a; } };
+struct Square    { template <class T> T operator()(T a) { return a * a; } };
+struct Cube      { template <class T> T operator()(T a) { return a * a * a; } };
+struct Pow4      { template <class T> T operator()(T a) { return Square()(a) * Square()(a); } };
+struct Absolute  { template <class T> T operator()(T a) { return abs(a); } };
+struct Logarithm { template <class T> T operator()(T a) { return log(a); } };
+
+struct AddAbs      { template <class T> T operator()(T sum, T a) { return sum + Absolute()(a); } };
+struct AddSquare   { template <class T> T operator()(T sum, T a) { return sum + Square()(a); } };
+struct AddLog      { template <class T> T operator()(T sum, T a) { return sum + Logarithm()(a); } };
+
+// Length
+
+template <class T> double statLength(T input, size_t size)
+{
+    return static_cast<double>(size);
+}
+
+// Min / Max Values
+
+template <class T> double statMin(T input, size_t size)
+{
+    return *(std::min_element(input, input + size));
+}
+
+template <class T> double statMax(T input, size_t size)
+{
+    return *(std::max_element(input, input + size));
+}
+
+// Counts
+
+template <class T, typename CountOp> double statCount(T input, size_t size, CountOp op)
+{
+    size_t count = 0;
+    
+    for (size_t i; i < size; i++)
+        if (op(input[i]))
+            count++;
+    
+    return count;
+}
+
+template <class T, typename Op>
+struct FixedCompare
+{
+    FixedCompare(T value, Op op) : mValue(value) {}
+    bool operator()(T a) { return Op()(a, mValue); }
+    T mValue;
+};
+
+template <class T, class U> double statCountAbove(T input, U threshold, size_t size)
+{
+    return statCount(input, size, FixedCompare<U, std::greater<U> >(threshold));
+}
+
+template <class T, class U> double statCountBelow(T input, U threshold, size_t size)
+{
+    return statCount(input, size, FixedCompare<U, std::less<U> >(threshold));
+}
+
+// Ratios
+
+template <class T, class U> double statRatioAbove(T input, U threshold, size_t size)
+{
+    return statCountAbove(input, threshold, size) / statLength(input, size);
+}
+
+template <class T, class U> double statRatioBelow(T input, U threshold, size_t size)
+{
+    return statCountBelow(input, threshold, size) / statLength(input, size);
+}
+
+// Sums
+
+template <class T> double statSum(T input, size_t size)
+{
+    return std::accumulate(input, input + size, 0.0);
+}
+
+template <class T> double statSumAbs(T input, size_t size)
+{
+    return std::accumulate(input, input + size, 0.0, AddAbs());
+}
+
+template <class T> double statSumSquares(T input, size_t size)
+{
+    return std::accumulate(input, input + size, 0.0, AddSquare());
+}
+
+template <class T> double statSumLogs(T input, size_t size)
+{
+    return std::accumulate(input, input + size, 0.0, AddLog());
+}
+
+// Weighted Sums
+
+template <class T, typename Op, typename WeightOp> double statWeightedSum(T input, size_t size, Op op, WeightOp weight)
+{
+    double sum = 0.0;
+    
+    for (size_t i = 0; i < size; i++)
+        sum += weight(i) * op(input[i]);
+    
+    return sum;
+}
+
+template <class T> double statWeightedSum(T input, size_t size)
+{
+    return statWeightedSum(input, size, Pass());
+}
+
+template <class T> double statWeightedSumAbs(T input, size_t size)
+{
+    return statWeightedSum(input, size, Absolute());
+}
+
+template <class T> double statWeightedSumSquares(T input, size_t size)
+{
+    return statWeightedSum(input, size, Square());
+}
+
+template <class T> double statWeightedSumLogs(T input, size_t size)
+{
+    return statWeightedSum(input, size, Logarithm());
+}
+
+// Weighted Sums (by weights)
+
+template <class T, typename Op> double statWeightedSum(T input, T weights, size_t size, Op op)
+{
+    struct getWeight
+    {
+        getWeight(T weights) : mWeights(weights) {}
+        double operator()(size_t i) { return mWeights[i]; }
+        T mWeights;
+    };
+    
+    return statWeightedSum(input, size, op, getWeight(weights));
+}
+
+template <class T> double statWeightedSum(T input, T weights, size_t size)
+{
+    return statWeightedSum(input, weights, size, Pass());
+}
+
+template <class T> double statWeightedSumAbs(T input, T weights, size_t size)
+{
+    return statWeightedSum(input, weights, size, Absolute());
+}
+
+template <class T> double statWeightedSumSquares(T input, T weights, size_t size)
+{
+    return statWeightedSum(input, weights, size, Square());
+}
+
+template <class T> double statWeightedSumLogs(T input, T weights, size_t size)
+{
+    return statWeightedSum(input, weights, size, Logarithm());
+}
+
+// Product
+
+template <class T> double statProduct(T input, size_t size)
+{
+    return std::accumulate(input, input + size, 1, std::multiplies<double>());
+}
+
+// Means
+
+template <class T> double statMean(T input, size_t size)
+{
+    return statSum(input, size) / statLength(input, size);
+}
+
+template <class T> double statGeometricMean(T input, size_t size)
+{
+    return exp(statSumLogs(input, size) / statLength(input, size));
+}
+
+// Variance
+
+template <class T> double statVariance(T input, size_t size)
+{
+    struct AddSquareDifference
+    {
+        AddSquareDifference(double value) : mValue(value) {}
+        double operator()(double sum, double a) { return sum + Square()(a - mValue); }
+        double mValue;
+    };
+
+    return std::accumulate(input, input + size, 0.0, AddSquareDifference(statMean(input, size)));
+}
+                           
+// Standard Deviation
+
+template <class T> double statStandardDeviation(T input, size_t size)
+{
+    return sqrt(statVariance(input, size));
+}
+
+// Shape
+
+template <class T> double statCentroid(T input, size_t size)
+{
+    return statWeightedSum(input, size) / statSum(input, size);
+}
+                        
+template <class T> double statSpread(T input, size_t size)
+{
+    struct Weight
+    {
+        Weight(double value) : mValue(value) {}
+        double operator()(size_t i) { return Square()(static_cast<double>(i) - mValue); }
+        double mValue;
+    };
+    
+    return statWeightedSum(input, size, Pass(), Weight(statCentroid(input, size)));
+}
+
+template <class T> double statSkewness(T input, size_t size)
+{
+    struct Weight
+    {
+        Weight(double value) : mValue(value) {}
+        double operator()(size_t i) { return Cube()(static_cast<double>(i) - mValue); }
+        double mValue;
+    };
+
+    return statWeightedSum(input, size, Pass(), Weight(statCentroid(input, size))) / Cube(sqrt(statSpread(input, size)));
+}
+
+template <class T> double statKurtosis(T input, size_t size)
+{
+    struct Weight
+    {
+        Weight(double value) : mValue(value) {}
+        double operator()(size_t i) { return Pow4()(static_cast<double>(i) - mValue); }
+        double mValue;
+    };
+    
+    return statWeightedSum(input, size, Pass(), Weight(statCentroid(input, size))) / square(statSpread(input, size));
+}
+                        
+// Flatness
+
+template <class T> double statFlatness(T input, size_t size)
+{
+    return statGeometricMean(input, size) / statMean(input, size);
+}
+
+// RMS
+
+template <class T> double statRMS(T input, size_t size)
+{
+    return sqrt(statSumSquares(input, size));
+}
+
+// Crest
+
+template <class T> double statCrest(T input, size_t size)
+{
+    return statMax(input, size) / statSumSquares(input, size);
+}
+
+#endif /* Statistics_h */
--- FrameLib_Objects/Vector/FrameLib_Vector_Objects.h
+++ FrameLib_Objects/Vector/FrameLib_Vector_Objects.h
@@ -3,26 +3,26 @@
 #define FRAMELIB_VECTOR_OBJECTS_H
 
 #include "FrameLib_Vector_Template.h"
-#include "FrameLib_Vector_Functions.h"
+#include "../../FrameLib_Dependencies/Statistics.hpp"
 
 // Info specialisations
 
-template<> const char *FrameLib_Vector<vectorMin>::getOpString()            { return "minimum"; }
-template<> const char *FrameLib_Vector<vectorMax>::getOpString()            { return "maximum"; }
-template<> const char *FrameLib_Vector<vectorSum>::getOpString()            { return "sum"; }
-template<> const char *FrameLib_Vector<vectorProduct>::getOpString()        { return "product"; }
-template<> const char *FrameLib_Vector<vectorMean>::getOpString()           { return "mean"; }
-template<> const char *FrameLib_Vector<vectorGeometricMean>::getOpString()  { return "geometric mean"; }
-template<> const char *FrameLib_Vector<vectorLength>::getOpString()         { return "length"; }
+template<> const char *FrameLib_Vector<statMin>::getOpString()            { return "minimum"; }
+template<> const char *FrameLib_Vector<statMax>::getOpString()            { return "maximum"; }
+template<> const char *FrameLib_Vector<statSum>::getOpString()            { return "sum"; }
+template<> const char *FrameLib_Vector<statProduct>::getOpString()        { return "product"; }
+template<> const char *FrameLib_Vector<statMean>::getOpString()           { return "mean"; }
+template<> const char *FrameLib_Vector<statGeometricMean>::getOpString()  { return "geometric mean"; }
+template<> const char *FrameLib_Vector<statLength>::getOpString()         { return "length"; }
 
 // Type definitions
 
-typedef FrameLib_Vector<vectorMin>              FrameLib_VectorMin;
-typedef FrameLib_Vector<vectorMax>              FrameLib_VectorMax;
-typedef FrameLib_Vector<vectorSum>              FrameLib_Sum;
-typedef FrameLib_Vector<vectorProduct>          FrameLib_Product;
-typedef FrameLib_Vector<vectorMean>             FrameLib_Mean;
-typedef FrameLib_Vector<vectorGeometricMean>    FrameLib_GeometricMean;
-typedef FrameLib_Vector<vectorLength>           FrameLib_Length;
+typedef FrameLib_Vector<statMin>              FrameLib_VectorMin;
+typedef FrameLib_Vector<statMax>              FrameLib_VectorMax;
+typedef FrameLib_Vector<statSum>              FrameLib_Sum;
+typedef FrameLib_Vector<statProduct>          FrameLib_Product;
+typedef FrameLib_Vector<statMean>             FrameLib_Mean;
+typedef FrameLib_Vector<statGeometricMean>    FrameLib_GeometricMean;
+typedef FrameLib_Vector<statLength>           FrameLib_Length;
 
 #endif
