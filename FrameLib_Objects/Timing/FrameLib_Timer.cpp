933180df518a4deeb5afcdcadc5a157a0c5fe7e8
Alex Harker <ajharker@gmail.com>
2017-10-11 03:19:09 +0100

    Added timer object (- docs)

---
+++ FrameLib_Objects/Timing/FrameLib_Timer.cpp
@@ -0,0 +1,76 @@
+
+#include "FrameLib_Timer.h"
+
+FrameLib_Timer::FrameLib_Timer(FrameLib_Context context, FrameLib_Parameters::Serial *serialisedParameters, void *owner) : FrameLib_Processor(context, owner, &sParamInfo, 2, 1)
+{
+    mParameters.addEnum(kUnits, "units", 0);
+    mParameters.addEnumItem(kSamples, "samples");
+    mParameters.addEnumItem(kMS, "ms");
+    mParameters.addEnumItem(kSeconds, "seconds");
+    mParameters.setInstantiation();
+    
+    mParameters.set(serialisedParameters);
+    
+    setInputMode(0, false, false, false);
+    calculateMultiplier();
+}
+
+// Info
+
+std::string FrameLib_Timer::objectInfo(bool verbose)
+{
+    return formatInfo("Outputs the current time in the specified units: Output is a single value.",
+                   "Outputs the current time in the specified units.", verbose);
+}
+
+std::string FrameLib_Timer::inputInfo(unsigned long idx, bool verbose)
+{
+    return formatInfo("Trigger Input - input frames generate output", "Trigger Input", verbose);
+}
+
+std::string FrameLib_Timer::outputInfo(unsigned long idx, bool verbose)
+{
+    return "Output Values";
+}
+
+// Parameter Info
+
+FrameLib_Timer::ParameterInfo FrameLib_Timer::sParamInfo;
+
+FrameLib_Timer::ParameterInfo::ParameterInfo()
+{
+    add("Sets the time units used to for output.");
+}
+
+// Calculate Multiplier
+
+void FrameLib_Timer::calculateMultiplier()
+{
+    switch ((Units) (mParameters.getValue(kUnits)))
+    {
+        case kMS:       mMultiplier = 1000.0 / FrameLib_TimeFormat(mSamplingRate);   break;
+        case kSeconds:  mMultiplier = 1.0 / FrameLib_TimeFormat(mSamplingRate);      break;
+        case kSamples:  mMultiplier = FrameLib_TimeFormat(1);                       break;
+    }
+}
+
+// Update and Process
+
+void FrameLib_Timer::update()
+{
+    if (mParameters.changed(kUnits))
+        calculateMultiplier();
+}
+
+void FrameLib_Timer::process()
+{
+    requestOutputSize(0, 1);
+    
+    if (allocateOutputs())
+    {
+        unsigned long size;
+        double *output = getOutput(0, &size);
+        
+        output[0] = (getCurrentTime() - getInputFrameTime(0)) * mMultiplier;
+    }
+}
---
+++ FrameLib_Objects/Timing/FrameLib_Timer.h
@@ -0,0 +1,50 @@
+
+#ifndef FRAMELIB_TIMER_H
+#define FRAMELIB_TIMER_H
+
+#include "FrameLib_DSP.h"
+
+class FrameLib_Timer : public FrameLib_Processor
+{
+    // Parameter Enums and Info
+
+    enum ParameterList { kUnits };
+    enum Units { kSamples, kMS, kSeconds };
+
+    struct ParameterInfo : public FrameLib_Parameters::Info { ParameterInfo(); };
+
+public:
+    
+    // Constructor
+    
+    FrameLib_Timer(FrameLib_Context context, FrameLib_Parameters::Serial *serialisedParameters, void *owner);
+    
+    // Info
+    
+    std::string objectInfo(bool verbose);
+    std::string inputInfo(unsigned long idx, bool verbose);
+    std::string outputInfo(unsigned long idx, bool verbose);
+    
+private:
+    
+    // Calculate Multiplier
+    
+    void calculateMultiplier();
+    
+    // Object Reset
+    
+    void objectReset() { calculateMultiplier(); }
+    
+    // Update and Process
+    
+    void update();
+    void process();
+    
+    // Data
+
+    FrameLib_TimeFormat mMultiplier;
+
+    static ParameterInfo sParamInfo;
+};
+
+#endif
