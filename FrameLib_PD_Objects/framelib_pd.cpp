a8f018558f853c9c81b8dba7d2bf8395b8c2b930
Alex Harker <ajharker@gmail.com>
2018-03-26 17:04:40 +0100

    Starting point for framelib pd building in one file

---
+++ FrameLib_PD_Objects/framelib_pd.cpp
@@ -0,0 +1,20 @@
+
+#include "FrameLib_Interval.h"
+#include "FrameLib_Source.h"
+#include "FrameLib_Sink.h"
+#include "FrameLib_Unary_Objects.h"
+#include "FrameLib_Binary_Objects.h"
+#include "FrameLib_PDClass.h"
+
+extern "C" int main(void)
+{
+    FrameLib_PDClass<FrameLib_Expand <FrameLib_Interval> >::makeClass("fl.interval~");
+    FrameLib_PDClass<FrameLib_Expand <FrameLib_Sink> >::makeClass("fl.sink~");
+    FrameLib_PDClass<FrameLib_Expand <FrameLib_Source> >::makeClass("fl.source~");
+    
+    
+    FrameLib_PDClass<FrameLib_Expand <FrameLib_Plus> >::makeClass("fl.+~");
+    FrameLib_PDClass<FrameLib_Expand <FrameLib_Minus> >::makeClass("fl.-~");
+    FrameLib_PDClass<FrameLib_Expand <FrameLib_Multiply> >::makeClass("fl.*~");
+    FrameLib_PDClass<FrameLib_Expand <FrameLib_Divide> >::makeClass("fl./~");
+}
