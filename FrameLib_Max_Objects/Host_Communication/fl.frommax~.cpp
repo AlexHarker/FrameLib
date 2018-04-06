1f23cfe51ae8b43b1cae6365771cb91b24b195ca
Alex Harker <ajharker@gmail.com>
2018-04-06 16:15:26 +0100

    Remove headers from max host communication objects and place in the cpp files

--- FrameLib_Max_Objects/Host_Communication/fl.frommax~.cpp
+++ FrameLib_Max_Objects/Host_Communication/fl.frommax~.cpp
@@ -1,5 +1,34 @@
 
-#include "fl.frommax~.h"
+#include "FrameLib_FromHost.h"
+#include <FrameLib_MaxClass.h>
+
+// Max Class
+
+class FrameLib_MaxClass_FromMax : public FrameLib_MaxClass<FrameLib_Expand<FrameLib_FromHost> >
+{
+    struct FromHostProxy : public FrameLib_FromHost::Proxy, public FrameLib_MaxProxy {};
+    
+public:
+    
+    // Class Initialisation
+    
+    static void classInit(t_class *c, t_symbol *nameSpace, const char *classname);
+    
+    // Constructor
+    
+    FrameLib_MaxClass_FromMax(t_symbol *s, long argc, t_atom *argv);
+    
+    // Additional handlers
+    
+    void intHandler(t_atom_long in);
+    void floatHandler(double in);
+    void list(t_symbol *s, long argc, t_atom *argv);
+    void anything(t_symbol *s, long argc, t_atom *argv);
+    
+private:
+    
+    FromHostProxy *mProxy;
+};
 
 // Max Class
 
--- FrameLib_Max_Objects/Host_Communication/fl.tomax~.cpp
+++ FrameLib_Max_Objects/Host_Communication/fl.tomax~.cpp
@@ -1,5 +1,48 @@
 
-#include "fl.tomax~.h"
+#include "FrameLib_ToHost.h"
+#include <FrameLib_MaxClass.h>
+#include <algorithm>
+
+// Max Class
+
+class FrameLib_MaxClass_ToMax : public FrameLib_MaxClass<FrameLib_Expand<FrameLib_ToHost> >
+{
+    struct ToHostProxy : public FrameLib_ToHost::Proxy, public FrameLib_MaxProxy
+    {
+        ToHostProxy(FrameLib_MaxClass_ToMax *object) : mObject(object){}
+        
+        virtual void output(const double *values, unsigned long N);
+        virtual void output(const FrameLib_Parameters::Serial *serial);
+        
+    private:
+        
+        unsigned long limitSize(unsigned long N) { return std::min(N, 32767UL); }
+        
+        FrameLib_MaxClass_ToMax *mObject;
+    };
+    
+public:
+    
+    // Class Initialisation
+    
+    static void classInit(t_class *c, t_symbol *nameSpace, const char *classname);
+    
+    // Constructor
+    
+    FrameLib_MaxClass_ToMax(t_symbol *s, long argc, t_atom *argv);
+    
+    // Send to the Outlet
+    
+    static void toOutletExternal(FrameLib_MaxClass_ToMax *x, t_symbol *s, short ac, t_atom *av) { x->toOutlet(s, ac, av); }
+    
+    void toOutlet(t_symbol *s, short ac, t_atom *av);
+    
+private:
+    
+    // Data
+    
+    void *mOutlet;
+};
 
 // Proxy Class
 
