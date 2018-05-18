70b130ebfb3ba883cdfa0d634bde3995ce633a26
Owen Green <gungwho@gmail.com>
2018-05-18 11:27:14 +0100

    Branch for NanFilter pull request

---
+++ FrameLib_Objects/Vector/FrameLib_NanFilter.cpp
@@ -0,0 +1,88 @@
+#include "FrameLib_NanFilter.h"
+// Constructor
+
+FrameLib_NanFilter::FrameLib_NanFilter(FrameLib_Context context, FrameLib_Parameters::Serial *serialisedParameters, FrameLib_Proxy *proxy) : FrameLib_Processor(context, proxy, &sParamInfo, 2, 1)
+{
+    mParameters.addEnum(enum_value(ParameterList::kMode), "mode");
+    mParameters.addEnumItem(enum_value(Modes::kReplace), "replace");
+    mParameters.addEnumItem(enum_value(Modes::kRemove), "remove");
+
+    mParameters.addDouble(enum_value(ParameterList::kValue), "value",0);
+    
+    mParameters.set(serialisedParameters);
+    setParameterInput(1);
+}
+
+// Info
+
+std::string FrameLib_NanFilter::objectInfo(bool verbose)
+{
+    return formatInfo("Replaces NaNs in a vector",
+                      "Replaces NaNs in a vector", verbose);
+}
+
+std::string FrameLib_NanFilter::inputInfo(unsigned long idx, bool verbose)
+{
+    return formatInfo("Input Frames", "Input Frames", verbose);
+}
+
+std::string FrameLib_NanFilter::outputInfo(unsigned long idx, bool verbose)
+{
+    return "Vector with NaNs replaced";
+}
+
+// Parameter Info
+
+FrameLib_NanFilter::ParameterInfo FrameLib_NanFilter::sParamInfo;
+
+FrameLib_NanFilter::ParameterInfo::ParameterInfo()
+{
+    add("Set whether to replace or remove NaN values" );
+    add("Set the value with which to replace NaN values");
+    
+}
+
+// Process
+void FrameLib_NanFilter::process()
+{
+    unsigned long sizeIn;
+    unsigned long sizeOut = 0;
+    Modes mode = enum_lookup<Modes>( mParameters.getInt((enum_value(ParameterList::kMode))));
+    double replacement = mParameters.getValue(enum_value(ParameterList::kValue));
+    const double *input = getInput(0, &sizeIn);
+    
+    switch(mode)
+    {
+        case Modes::kReplace:
+        {
+            requestOutputSize(0, sizeIn);
+            if (allocateOutputs())
+            {
+                double* output = getOutput(0, &sizeOut);
+                
+                std::transform(input, input+sizeOut, output, [&replacement](double val)
+                {
+                    return std::isnan(val) ? replacement : val;
+                });
+            }
+            break;
+        }
+        
+        default: //remove
+        {
+            double* tmp = alloc<double>(sizeIn);
+            
+            double* tmp_end = std::copy_if(input, input + sizeIn, tmp,[](double val){
+                return !std::isnan(val);
+            });
+            
+            requestOutputSize(0, (size_t)(tmp_end - tmp));
+            if(allocateOutputs())
+            {
+                double* output = getOutput(0, &sizeOut);
+                copyVector(output, tmp, sizeOut); 
+            }
+            dealloc(tmp);
+        }
+    }
+}
