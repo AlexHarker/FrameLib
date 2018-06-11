cd7e43d5c488de9a629868f1d7898fdab4f4bda3
Alex Harker <ajharker@gmail.com>
2018-06-11 15:08:47 +0100

    Updated read me

---
+++ FrameLib_Framework/FrameLib_ErrorReporting.h
@@ -0,0 +1,12 @@
+//
+//  FrameLib_ErrorReporting.h
+//  framelib~
+//
+//  Created by Alex Harker on 10/06/2018.
+//
+
+#ifndef FrameLib_ErrorReporting_h
+#define FrameLib_ErrorReporting_h
+
+
+#endif /* FrameLib_ErrorReporting_h */
