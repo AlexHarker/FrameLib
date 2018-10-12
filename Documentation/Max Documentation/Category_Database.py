332d8aaca78e3485d122b27ed38d37568659d201
James Bradbury <jamesbradbury93@gmail.com>
2018-10-12 20:54:23 +0100

    py file to create an object category database

--- Documentation/Max Documentation/Category_Database.py
+++ Documentation/Max Documentation/Category_Database.py
@@ -1,7 +1,27 @@
 import os
+import json
+from strippers import strip_extension
 
+category_dict = dict({})
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-dir_path = dir_path.replace('/Documentation/Max Documentation', '/FrameLib_Max_Objects')
+object_path = dir_path.replace('/Documentation/Max Documentation', '/FrameLib_Max_Objects')
+output_path = f'{dir_path}/category_database.json'
 
-print(os.walk(dir_path))
+file_categories = (os.listdir(object_path))
+file_categories.remove('_MaxSDK_')
+file_categories.remove('.DS_Store')
+file_categories.remove('Common')
+file_categories.sort()
+
+for item in file_categories:
+    files = os.listdir(f'{object_path}/{item}')
+    if 'ibuffer' in files:
+        files.remove('ibuffer')
+    for i in range(len(files)):
+        files[i] = strip_extension(files[i], 1)
+    category_dict[item] = files
+    
+with open(output_path, 'w+') as fp:
+    json.dump(category_dict, fp, indent=4)
+
