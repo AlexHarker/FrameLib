55bef4a3c7b57ea2625c5787eae7b3c77f611923
James Bradbury <jamesbradbury93@gmail.com>
2018-10-12 21:03:55 +0100

    Updated master python script to include category building

--- Documentation/Max Documentation/Build_All_Files.py
+++ Documentation/Max Documentation/Build_All_Files.py
@@ -1,36 +1,34 @@
 import parse_to_dlookup
 import parse_to_qlookup
 import parse_to_tlookup
+import create_category_database
 import create_tutorial_coll
 
 def main():
+    
+    print('*' * 40)
+    print('Building Category Database')
+    create_category_database.main()
+    print('*' * 40)
 
     print('*' * 40)
     print('Building dlookup')
     parse_to_dlookup.main()
-    print('Finished dlookup')
-    print('*' * 40)
     print(' ' * 40)
 
     print('*' * 40)
     print('Building qlookup')
     parse_to_qlookup.main()
-    print('Finished dlookup')
-    print('*' * 40)
     print(' ' * 40)
 
     print('*' * 40)
     print('Building tlookup')
     parse_to_tlookup.main()
-    print('Finished tlookup')
-    print('*' * 40)
     print(' ' * 40)
 
     print('*' * 40)
     print('Building tutorial name coll')
     create_tutorial_coll.main()
-    print('Finished tutorial name coll')
-    print('*' * 40)
     print(' ' * 40)
 
     print(' ' * 40)
