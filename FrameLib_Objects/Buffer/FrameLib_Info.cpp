78bb81db82ab0b5c703f11117adf77bdf1419d01
James Bradbury <jamesbradbury93@gmail.com>
2018-11-30 17:16:12 +0000

    Removed tildes from info strings

--- FrameLib_Objects/Buffer/FrameLib_Info.cpp
+++ FrameLib_Objects/Buffer/FrameLib_Info.cpp
@@ -29,8 +29,8 @@ FrameLib_Info::FrameLib_Info(FrameLib_Context context, FrameLib_Parameters::Seri
 
 std::string FrameLib_Info::objectInfo(bool verbose)
 {
-    return formatInfo("Outputs the sample rate, number of channels and size of a buffer (in milliseconds)~. The size units can be changed.",
-                      "Outputs the sample rate, channels and size of a buffer~.", verbose);
+    return formatInfo("Outputs the sample rate, number of channels and size of a buffer (in milliseconds). The size units can be changed.",
+                      "Outputs the sample rate, channels and size of a buffer.", verbose);
 }
 
 std::string FrameLib_Info::inputInfo(unsigned long idx, bool verbose)
@@ -52,7 +52,7 @@ FrameLib_Info::ParameterInfo FrameLib_Info::sParamInfo;
 
 FrameLib_Info::ParameterInfo::ParameterInfo()
 {
-    add("Sets the buffer~ name to use.");
+    add("Sets the buffer name to use.");
     add("Sets the units of output for the size of the buffer.");
 }
 
