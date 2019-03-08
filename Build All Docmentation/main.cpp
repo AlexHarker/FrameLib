3549758777c6101928197d7453501330661c7376
James Bradbury <jamesbradbury93@gmail.com>
2019-03-08 16:25:45 +0000

    Xcode can build python target separately, or as a post-script for the c++ terminal app

---
+++ Build All Docmentation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Build All Docmentation
+//
+//  Created by James Bradbury on 08/03/2019.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
