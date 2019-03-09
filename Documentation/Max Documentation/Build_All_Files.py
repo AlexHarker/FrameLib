76759b407af39719608dd8f1cf0feed97d5ea13e
James Bradbury <jamesbradbury93@gmail.com>
2019-03-09 18:25:32 +0000

    Easier to read printing

--- Documentation/Max Documentation/Build_All_Files.py
+++ Documentation/Max Documentation/Build_All_Files.py
@@ -4,44 +4,39 @@ import parse_to_tlookup
 import create_category_database
 import create_tutorial_coll
 import edit_raw_XML
+import helpers as hp
 
 def main():
+    root = hp.get_path()
+    hp.sign_off()
+    hp.space()
     
-    print('*' * 40)
-    print('Building Category Database')
-    create_category_database.main()
-    print(' ' * 40)
+    print('1. Building Category Database')
+    create_category_database.main(root)
+    hp.hyp()
 
-    print('*' * 40)
-    print('Editing XML Files')
-    edit_raw_XML.main()
-    print(' ' * 40)
+    print('2. Editing XML Files')
+    edit_raw_XML.main(root)
+    hp.hyp()
 
-    print('*' * 40)
-    print('Building dlookup')
-    parse_to_dlookup.main()
-    print(' ' * 40)
+    print('3. Building dlookup')
+    parse_to_dlookup.main(root)
+    hp.hyp()
 
-    print('*' * 40)
-    print('Building qlookup')
-    parse_to_qlookup.main()
-    print(' ' * 40)
+    print('4. Building qlookup')
+    parse_to_qlookup.main(root)
+    hp.hyp()
 
-    print('*' * 40)
-    print('Building tlookup')
-    parse_to_tlookup.main()
-    print(' ' * 40)
+    print('5. Building tlookup')
+    parse_to_tlookup.main(root)
+    hp.hyp()
 
-    print('*' * 40)
-    print('Building tutorial name coll')
-    create_tutorial_coll.main()
-    print(' ' * 40)
+    print('6. Building tutorial name coll')
+    create_tutorial_coll.main(root)
+    hp.hyp()
+    print(' ')
+    print("Completed all python scripts!!!")
 
-    print(' ' * 40)
-    print("_-" * 40)
-    print(' ' + "Completed all python files")
-    print("-_" * 40)
-    print(' ' * 40)
 
 if __name__ == '__main__':
     main()
