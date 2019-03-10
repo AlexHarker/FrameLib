c577f218aa83c4f07d21f122005b47d5e4894529
James Bradbury <jamesbradbury93@gmail.com>
2019-03-10 14:36:49 +0000

    made prefixes for file to make ordering more visible

--- Documentation/Max Documentation/_Build_All_Files.py
+++ Documentation/Max Documentation/_Build_All_Files.py
@@ -1,9 +1,9 @@
-import parse_to_dlookup
-import parse_to_qlookup
-import parse_to_tlookup
-import create_category_database
-import create_tutorial_coll
-import edit_raw_XML
+import a_create_category_database
+import b_edit_raw_XML
+import c_parse_to_dlookup
+import d_parse_to_qlookup
+import e_parse_to_tlookup
+import f_create_tutorial_coll
 import helpers as hp
 
 def main():
@@ -12,27 +12,27 @@ def main():
     hp.space()
     
     print('1. Building Category Database')
-    create_category_database.main(root)
+    a_create_category_database.main(root)
     hp.hyp()
 
     print('2. Editing XML Files')
-    edit_raw_XML.main(root)
+    b_edit_raw_XML.main(root)
     hp.hyp()
 
     print('3. Building dlookup')
-    parse_to_dlookup.main(root)
+    c_parse_to_dlookup.main(root)
     hp.hyp()
 
     print('4. Building qlookup')
-    parse_to_qlookup.main(root)
+    d_parse_to_qlookup.main(root)
     hp.hyp()
 
     print('5. Building tlookup')
-    parse_to_tlookup.main(root)
+    e_parse_to_tlookup.main(root)
     hp.hyp()
 
     print('6. Building tutorial name coll')
-    create_tutorial_coll.main(root)
+    f_create_tutorial_coll.main(root)
     hp.hyp()
     print(' ')
     print("Completed all python scripts!!!")
