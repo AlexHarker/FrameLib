16a28151d962036a5696ecba5ca1d680452fe4f3
James Bradbury <jamesbradbury93@gmail.com>
2019-06-14 17:02:15 +0100

    Terse documentation of each stage

--- Documentation/Max Documentation/_Build_All_Files.py
+++ Documentation/Max Documentation/_Build_All_Files.py
@@ -12,30 +12,47 @@ def main():
     hp.sign_off()
     hp.space()
     
+    ## Stage 0
+    ## There is a prior stage here where make_object_list.py is called by Xcode.
+    ## This produces the header file which Build_Max_Docs.cpp uses to know about FrameLib objects and types.
+    
+    ## Creates a category database in .json format.
+    ## The JSON file is used by edit_raw_XML.py to assign object categories to the xml files.
     print('1. Building Category Database')
     a_create_category_database.main(root)
     hp.hyp()
 
+    ## The purpose of this script is to set the categories for the Raw_XML files. 
+    ## C++ doesnt know about the categories at XML creation and its easier to iterate file structures in python.
+    ## Edited XML files are copied from /tmp/ to the refpages directory
     print('2. Editing XML Files')
     b_edit_raw_XML.main(root)
     hp.hyp()
 
+    ## This script creates a dictionary used to display specific object info in the extras Max Patch.
+    ## Similar to the qlookup, but is specifically used to display the digest with mouse hovering
     print('3. Building dlookup')
     c_parse_to_dlookup.main(root)
     hp.hyp()
 
+    ## This script creates a dictionary that contains specific object information.
+    ## This provides the dynamic hover behaviour
     print('4. Building qlookup')
     d_parse_to_qlookup.main(root)
     hp.hyp()
 
+    ## Creates a dictionary used to display names and descriptions of tutorials in the extras Max Patch.
+    ## The tutorials are categorised by difficulty. {Beginner, Intermediate, Advanced}
     print('5. Building tlookup')
     e_parse_to_tlookup.main(root)
     hp.hyp()
 
+    ## Creates a dict containing information about object parameters. This is used by the help file template.
     print('6. Building jlookup')
     f_parse_to_jlookup.main(root)
     hp.hyp()
 
+    ## Creates a coll containing the file names of the tutorials. Makes it a bit easier to load them.
     print('7. Building tutorial name coll')
     g_create_tutorial_coll.main(root)
     hp.hyp()
