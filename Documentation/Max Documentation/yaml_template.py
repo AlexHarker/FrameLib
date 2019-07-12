cd047375b197997677bd71c5da3d12d21851a3b9
James Bradbury <jamesbradbury93@gmail.com>
2019-07-12 16:11:02 +0100

    generates a simplistic yaml file with keywords and seealso

--- Documentation/Max Documentation/yaml_template.py
+++ Documentation/Max Documentation/yaml_template.py
@@ -13,18 +13,56 @@ root = get_path()
 package = cd_up(root, 2)
 max_objects = os.path.join(package, 'FrameLib_Max_Objects')
 
-f = open(os.path.join(root, 'object_relationships.yaml'), 'w+')
+prefilled_info = {
+
+    'Binary' : ['binary ops, operators'],
+    'Buffer' : ['buffer'],
+    'Complex_Binary' : ['complex numbers', 'complex operators'],
+    'Complex_Unary' : ['complex numbers', 'complex operators'],
+    'Expressions' : ['expression'],
+    'Filters' : ['filters'],
+    'Generators' : ['generators'],
+    'Host_Communication' : ['conversion'],
+    'IO' : ['conversion'],
+    'Mapping' : ['scaling', 'mapping'],
+    'Parameters' : ['parameters'],
+    'Routing' : ['routing'],
+    'Schedulers' : ['timing, schedulers', 'triggers'],
+    'Spatial' : ['spatialiastion'],
+    'Spectral' : ['FFT, windowing, frequency domain'],
+    'Storage' : ['storage'],
+    'Streaming' : ['multistreaming', 'streams'],
+    'Ternary' : ['ternary ops', 'operators'],
+    'Time_Smoothing' : ['time smoothing', 'statistics'],
+    'Timing' : ['timing'],
+    'Unary' : ['unary ops', 'operators'],
+    'Vector' : ['vector']
+}
+
+f = open(os.path.join(root, 'object_relationships.txt'), 'w+')
 all_objects = os.walk(max_objects)
 for root, dirs, files in all_objects:
-    for name in files:
-        name = os.path.splitext(name)[0]
+    for name, category in zip(files, root):
+        obj_name = os.path.splitext(name)[0]
+        category = os.path.basename(root)
         if name[:2] == 'fl':
-            f.write(f'{name}: \n')
+            other_files = os.listdir(root)
+            other_files.remove(name)
+            f.write(f'{obj_name}: \n')
+
             f.write(f'    keywords: \n')
+            for key in prefilled_info[category]:
+                f.write(f'        - {key} \n')
+            
+            f.write('\n')
+
             f.write(f'    seealso: \n')
+            for other_file in other_files:
+                other_file = os.path.splitext(other_file)[0]
+                f.write(f'        - {other_file} \n')
+            
             f.write('\n')
             
-
 f.close()
 
 
