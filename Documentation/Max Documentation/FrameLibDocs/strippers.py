9d0bdbf140a5b2a6f447f12a4e0852b495ad5559
James Bradbury <jamesbradbury93@gmail.com>
2019-08-14 20:46:18 +0100

    black formatting

--- Documentation/Max Documentation/FrameLibDocs/strippers.py
+++ Documentation/Max Documentation/FrameLibDocs/strippers.py
@@ -1,9 +1,11 @@
 #### Some useful functions that strip various substrings from strings ####
 
+
 def strip_space(tostrip):
     tostrip = tostrip.lstrip()
     tostrip = tostrip.rstrip()
     return tostrip
+
 
 def strip_extension(input_string, maxsplits):
     output = input_string.rsplit(".", maxsplits)[0]
--- Documentation/Max Documentation/FrameLibDocs/utils.py
+++ Documentation/Max Documentation/FrameLibDocs/utils.py
@@ -2,22 +2,25 @@ import os
 import random as rn
 import json
 
+
 def remove_ds(list_in):
-    if '.DS_Store' in list_in:
-        list_in.remove('.DS_Store')
+    if ".DS_Store" in list_in:
+        list_in.remove(".DS_Store")
     return list_in
 
+
 def thin_list(list_to_thin, things_to_thin):
-    '''
+    """
     Takes a list_to_thin and removes each element of things_to_thin from it (if it exists)
-    '''
+    """
     for badness in things_to_thin:
         if badness in list_to_thin:
             list_to_thin.remove(badness)
     return list_to_thin
 
+
 def write_json(json_file, in_dict):
-    '''
+    """
     Takes a dictionary and writes it to JSON file.
 
     Args:
@@ -25,75 +28,88 @@ def write_json(json_file, in_dict):
         in_dict: A dictionary that will be saved as JSON.
     Returns:
         None
-    '''
-    with open(json_file, 'w+') as fp:
+    """
+    with open(json_file, "w+") as fp:
         json.dump(in_dict, fp, indent=4)
 
+
 def read_json(json_file):
-    '''
+    """
     Takes a JSON file and returns a dictionary
 
     Args:
         json_file: A path to a JSON file that will be read.
     Returns:
         A python dictionary.
-    '''
-    with open(json_file, 'r') as fp:
+    """
+    with open(json_file, "r") as fp:
         data = json.load(fp)
         return data
 
+
 def cd_up(path, num):
-    '''
+    """
     Given path, traverse num directories up from it
-    '''
+    """
     t_path = path
     for _ in range(num):
         t_path = os.path.dirname(t_path)
     return t_path
 
+
 def get_path():
     return os.path.dirname(os.path.realpath(__file__))
 
+
 def ast():
-    '''
+    """
     Print some asterisks
-    '''
-    print('*' * 20)
+    """
+    print("*" * 20)
+
 
 def hyp():
-    '''
+    """
     Print some hyphens
-    '''
-    print('-' * 20)
+    """
+    print("-" * 20)
+
 
 def uds():
-    '''
+    """
     Print some underscores
-    '''
-    print('_' * 20)
+    """
+    print("_" * 20)
+
 
 def space():
-    '''
+    """
     Makes some space!
-    '''
-    print(' ' * 20)
-    print(' ' * 20)
+    """
+    print(" " * 20)
+    print(" " * 20)
+
 
 def orsym():
-    '''
+    """
     Prints a |
-    '''
-    print('|' * 20)
+    """
+    print("|" * 20)
+
 
 def sign_off():
-    '''
+    """
     Sign off the docs ;)
-    '''
+    """
     print(rn.choice(printouts))
-    print(' ')
-    print('A DSP framework for arbitrary size frame processing with arbitrary sub-sample accurate timing.')
-    print('Alex Harker, ajharker@gmail.com')
-    print('Max Documentation and Tutorials by James Bradbury, jamesbradbury93@gmail.com')
+    print(" ")
+    print(
+        "A DSP framework for arbitrary size frame processing with arbitrary sub-sample accurate timing."
+    )
+    print("Alex Harker, ajharker@gmail.com")
+    print(
+        "Max Documentation and Tutorials by James Bradbury, jamesbradbury93@gmail.com"
+    )
 
 
 printouts = [
@@ -140,7 +156,8 @@ printouts = [
    MM    `7                                         MM              MM        
    MM   d `7Mb,od8 ,6"Yb. `7MMpMMMb.pMMMb.  .gP"Ya  MM        `7MM  MM,dMMb.  
    MM""MM   MM' "'8)   MM   MM    MM    MM ,M   Yb MM          MM  MM    `Mb 
-   MM   Y   MM     ,pm9MM   MM    MM    MM 8M"""""" MM      ,   MM  MM     M8 
+   MM   Y   MM     ,pm9MM   MM    MM    MM 8M"""
+    """ MM      ,   MM  MM     M8 
    MM       MM    8M   MM   MM    MM    MM YM.    , MM     ,M   MM  MM.   ,M9 
  .JMML.   .JMML.  `Moo9^Yo.JMML  JMML  JMML.`Mbmmd.JMMmmmmMMM .JMML.P^YbmdP'
 """,
@@ -151,9 +168,5 @@ printouts = [
 |  _)( '__/'_` /' _ ` _ `\/'__`| |  _| | '_`\ 
 | |  | | ( (_| | ( ) ( ) (  ___| |_( | | |_) )
 (_)  (_) `\__,_(_) (_) (_`\____(____/(_(_,__/'
-"""
+""",
 ]
-
-
-
-                                                 
