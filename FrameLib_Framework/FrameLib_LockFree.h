d5895db66d1e03fbcb072e62a78bab0d0b85a036
Alex Harker <ajharker@gmail.com>
2019-08-20 11:14:57 +0100

    Complete lock free options so far

--- FrameLib_Framework/FrameLib_LockFree.h
+++ FrameLib_Framework/FrameLib_LockFree.h
@@ -19,10 +19,9 @@ public:
     struct Node
     {
         Node(T *owner)
-        : mOwner(owner), mNextInThread(nullptr), mNext(nullptr) {}
+        : mOwner(owner), mNext(nullptr) {}
         
         T *mOwner;
-        T *mNextInThread;
         Node *mNext;
     };
     
@@ -96,6 +95,138 @@ public:
     std::atomic<Pointer> mHead;
 };
 
+// N.B. - the current implementation of a lock-free queue is taken from:
+// Michael, Maged M., and Michael L. Scott.
+// Simple, Fast, and Practical Non-Blocking and Blocking Concurrent Queue Algorithms.
+// No. TR-600. ROCHESTER UNIV NY DEPT OF COMPUTER SCIENCE, 1995.
+
+
+template <class T>
+class FrameLib_LockFreeQueue
+{
+    
+public:
+    
+    struct Node
+    {
+        struct Pointer
+        {
+            Pointer() : mPointer(nullptr), mCount(0) {}
+            Pointer(Node *node, uintptr_t count) : mPointer(node), mCount(count) {}
+            
+            bool operator==(const Pointer& a) const
+            {
+                return a.mPointer == mPointer && a.mCount == mCount;
+            }
+            
+            // This structure must not have padding bits, so we use a pointer sized mCount
+            
+            Node *mPointer;
+            uintptr_t mCount;
+        };
+        
+        Node(T *owner)
+        : mOwner(owner), mNext(Pointer()) {}
+        
+        T *mOwner;
+        std::atomic<Pointer> mNext;
+    };
+
+    using NodePointer = typename Node::Pointer;
+    
+    FrameLib_LockFreeQueue() : mDummyNode(nullptr), mHead(NodePointer(&mDummyNode, 0)), mTail(NodePointer(&mDummyNode, 0)) {}
+    
+    void enqueue(Node *node)
+    {
+        node->mNext = NodePointer();
+        
+        while (true)
+        {
+            const NodePointer tail = mTail;
+            const NodePointer next = tail.mPointer->mNext;
+            
+            // Check that tail has not changed
+            
+            if (tail == mTail)
+            {
+                if (!next.mPointer)
+                {
+                    // If tail was pointing to the last node then we can attempt to enqueue
+                    
+                    if (compareAndSwap(tail.mPointer->mNext, next, NodePointer(node, next.mCount + 1)))
+                    {
+                        // Try to update tail to the inserted node
+                        
+                        compareAndSwap(mTail, tail, NodePointer(node, tail.mCount + 1));
+                        return;
+                    }
+                }
+                else
+                {
+                    // Try to advance tail if falling behind
+                    
+                    compareAndSwap(mTail, tail, NodePointer(next.mPointer, tail.mCount + 1));
+                }
+            }
+        }
+    }
+    
+    T *dequeue()
+    {
+        while (true)
+        {
+            // Read head, tail and next
+            
+            const NodePointer head = mHead.load(std::memory_order_relaxed);
+            const NodePointer tail = mTail.load(std::memory_order_relaxed);
+            const NodePointer next = head.mPointer->mNext.load(std::memory_order_relaxed);
+            
+            // Check that head has not changed
+            
+            if (head == mHead.load(std::memory_order_relaxed))
+            {
+                // Check if either the queue is empty or tail is falling behind
+                
+                if (head.mPointer == tail.mPointer)
+                {
+                    // Check for empty queue or try to advance tail if falling behind
+                    
+                    if (!next.mPointer)
+                        return nullptr;
+                    
+                    compareAndSwap(mTail, tail, NodePointer(next.mPointer, tail.mCount + 1));
+                }
+                else
+                {
+                    // Read the pointer before CAS otherwise another dequeue may free the next node
+                    
+                    T *object = next.mPointer->mOwner;
+                    
+                    // Attempt to change the head and complete dequeue
+                    
+                    if (compareAndSwap(mHead, head, NodePointer(next.mPointer, head.mCount + 1)))
+                    {
+                        head.mPointer->mNext.store(NodePointer(), std::memory_order_relaxed);
+                        return object;
+                    }
+                }
+            }
+        }
+        
+        return nullptr;
+
+    }
+    
+    Node mDummyNode;
+    std::atomic<NodePointer> mHead;
+    std::atomic<NodePointer> mTail;
+};
+
+
+
+
+
+
 template <class T>
 class FrameLib_AppleLockFreeQueue
 {
@@ -107,10 +238,9 @@ public:
     struct Node
     {
         Node(T *owner)
-        : mOwner(owner), mNextInThread(nullptr), mNext(nullptr) {}
+        : mOwner(owner), mNext(nullptr) {}
         
         T *mOwner;
-        T *mNextInThread;
         T *mNext;
     };
     
