62f94d2f9cc796472426956fb1b6085e8c69bfa0
James Bradbury <jamesbradbury93@gmail.com>
2019-08-23 18:45:21 +0100

    pass root from top level script to main function

--- Documentation/Max Documentation/8_cleanup.py
+++ Documentation/Max Documentation/8_cleanup.py
@@ -2,6 +2,6 @@ import os
 import shutil
 from FrameLibDocs.utils import cd_up, get_path
 
-root = cd_up(get_path(), 1)
-tmp = os.path.join(root, "__tmp__")
-shutil.rmtree(tmp)
+def main(root):
+    tmp = os.path.join(root, "__tmp__")
+    shutil.rmtree(tmp)
