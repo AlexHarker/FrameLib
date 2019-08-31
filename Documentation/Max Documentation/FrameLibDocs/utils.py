d94ad6e3bb480340c90e96cd6a9805d641cd267a
James Bradbury <jamesbradbury93@gmail.com>
2019-08-31 14:04:13 +0100

    function for reading YAML

--- Documentation/Max Documentation/FrameLibDocs/utils.py
+++ Documentation/Max Documentation/FrameLibDocs/utils.py
@@ -53,6 +53,13 @@ def read_json(json_file):
         data = json.load(fp)
         return data
 
+def read_yaml(yaml_file):
+    with open(yaml_file, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
 
 def cd_up(path, num):
     """
