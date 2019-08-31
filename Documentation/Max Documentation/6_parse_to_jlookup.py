a6f4f2017195af882b9bee55a968361169bc035a
James Bradbury <jamesbradbury93@gmail.com>
2019-08-31 14:07:04 +0100

    count from 1 for params

--- Documentation/Max Documentation/6_parse_to_jlookup.py
+++ Documentation/Max Documentation/6_parse_to_jlookup.py
@@ -18,43 +18,37 @@ class jParseAndBuild:
 
     def extract_from_refpage(self, x):
         self.tree = et.parse(x)
-        self.root = self.tree.getroot()  # c74object
+        self.root = self.tree.getroot()
         blank_param_dict = {}
         blank_internal = {}
         blank_descr = ""
         enums = ""
 
-        # Find Information #
+        # Find Information
         self.object_name = self.root.get("name")  # get the object name
-        param_idx = 0  # reset a variable to track the parameter number
+        param_idx = 1  # reset a variable to track the parameter number
         for child in self.root:  # iterate over the sections
             if child.tag == "misc":  # if the section is misc
-                if (
-                    child.get("name") == "Parameters"
-                ):  # if the section is misc and has name='Parameters'
+                if (child.get("name") == "Parameters"):  # if the section is misc and has name='Parameters'
                     for elem in child:  # for sub-sections
-                        blank_internal = {
-                            "name": elem.get("name")
-                        }  # store the name with the key/pair 'name'
+                        blank_internal = {"name": elem.get("name")}  # store the name with the key/pair 'name'
 
                         for description in elem:  # get the description out
                             blank_desc = strip_space(description.text)
 
+                            #### TODO ####
+                            # Edit the description if its multiline as is the case with fl.map~ #
+                            ##############
+
                             for bullet in description:  # if there are any bullet points
                                 if bullet.text != None:  # and its not none
-                                    if bullet.text[1] == "0":
+                                    if bullet.text[1] == "0": # if it is the first line it will be the title 'Parameter Options'
                                         blank_desc += f"\n\nParameter Options:"
 
                                     blank_desc += f"\n{bullet.text}"
-                        blank_internal[
-                            "description"
-                        ] = blank_desc  # set the description
+                        blank_internal["description"] = blank_desc  # set the description
 
-                        blank_param_dict[
-                            param_idx
-                        ] = (
-                            blank_internal
-                        )  # assign the blank_internal dict to a parameter number
+                        blank_param_dict[param_idx] = (blank_internal)  # assign the blank_internal dict to a parameter number
                         param_idx += 1
 
         param_dict = dict({self.object_name: blank_param_dict})
@@ -62,9 +56,6 @@ class jParseAndBuild:
         self.j_master_dict.update(param_dict)
 
         # once we've made the param stuff we append it to a dict with the name of the object
-        #
-
-
 # ----------- THE GUTS ----------- #
 
 
