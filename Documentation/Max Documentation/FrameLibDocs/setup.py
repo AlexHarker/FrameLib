ef5eca761c79cf47d154263a59b205fa1a92861b
James Bradbury <jamesbradbury93@gmail.com>
2019-09-04 12:29:47 +0100

    move to editable python package

---
+++ Documentation/Max Documentation/FrameLibDocs/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='FrameLibDocs',
+    version='1.0',
+    author='James Bradbury',
+    author_email='jamesbradbury93@gmail.com',
+    description='A package containing recipes used for auto-generation of FrameLib Documentation',
+    packages=['FrameLibDocs'],
+)
