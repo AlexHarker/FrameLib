faa2c785bddcc6b6b28cc66fbfc8b4c11a8d4402
Alex Harker <ajharker@gmail.com>
2019-11-17 16:42:56 +0000

    Unify expression parsing for max objects

--- FrameLib_Max_Objects/Expressions/expression_object_parsing.h
+++ FrameLib_Max_Objects/Expressions/expression_object_parsing.h
@@ -1,12 +1,127 @@
-//
-//  expression_object_parsing.h
-//  framelib~
-//
-//  Created by Alex Harker on 17/11/2019.
-//
 
-#ifndef expression_object_parsing_h
-#define expression_object_parsing_h
+#ifndef EXPRESSION_OBJECT_PARSING_H
+#define EXPRESSION_OBJECT_PARSING_H
 
+#include "FrameLib_MaxClass.h"
 
-#endif /* expression_object_parsing_h */
+// These objects parses their arguments differently to normal, which is handled by pre-parsing the atoms into a different format
+
+class ExprArgumentParser
+{
+    
+public:
+    
+    ExprArgumentParser(t_symbol *s, long argc, t_atom *argv, bool complex) : mSymbol(s)
+    {
+        if (argc && isStreamSpecifier(argv))
+        {
+            argc--;
+            mArgs.push_back(*argv++);
+        }
+        
+        concatenate(argc, argv, complex);
+        
+        while (argc--)
+        {
+            if (atom_getsym(argv) == gensym("/expr"))
+                concatenate(argc, ++argv, complex);
+            else
+                mArgs.push_back(*argv++);
+        }
+    }
+    
+    t_symbol *symbol() const    { return mSymbol;}
+    long count() const          { return static_cast<long>(mArgs.size());}
+    t_atom* args() const        { return const_cast<t_atom*>(mArgs.data()); }
+    
+private:
+    
+    // Stream Specifier Detector
+    
+    bool isStreamSpecifier(t_atom *a)
+    {
+        if (atom_gettype(a) == A_SYM)
+        {
+            t_symbol *sym = atom_getsym(a);
+            return strlen(sym->s_name) > 1 && sym->s_name[0] == '=';
+        }
+        
+        return false;
+    }
+    
+    // Tag Detection
+    
+    bool isValidTag(t_atom *a, bool complex)
+    {
+        t_symbol *sym = atom_getsym(a);
+        size_t len = strlen(sym->s_name);
+        
+        // Attribute
+        
+        if (len > 1 && sym->s_name[0] == '@')
+            return true;
+        
+        // Input tag
+        
+        if (len > 2 && sym->s_name[0] == '[' && sym->s_name[len - 1] == ']')
+            return true;
+        
+        // Basic parameter tag test
+        
+        if (len <= 1 || sym->s_name[0] != '/')
+            return false;
+        
+        // Escape division by known constants
+        
+        if ((complex && sym == gensym("/i")) || sym == gensym("/pi") || sym == gensym("/epsilon") || sym == gensym("/e") || sym == gensym("/inf"))
+            return false;
+        
+        // Require a parameter tag to have only letters after the slash
+        
+        for (const char *c = sym->s_name + 1; *c; c++)
+            if (!((*c >= 'a') && (*c <= 'z')) || ((*c >= 'A') && (*c <= 'Z')))
+                return false;
+        
+        return true;
+    }
+    
+    // Concatenator
+    
+    void concatenate(long& argc, t_atom*& argv, bool complex)
+    {
+        std::string concatenated;
+        
+        for (; argc && !isValidTag(argv, complex); argc--, argv++)
+        {
+            if (atom_gettype(argv) == A_SYM)
+                concatenated += atom_getsym(argv)->s_name;
+            else
+                concatenated += std::to_string(atom_getfloat(argv));
+            
+            // Add whitespace between symbols
+            
+            concatenated += " ";
+        }
+        
+        if (concatenated.length())
+        {
+            t_symbol *arg = gensym(concatenated.c_str());
+            mArgs.push_back(t_atom());
+            atom_setsym(&mArgs.back(), arg);
+        }
+    }
+    
+    t_symbol *mSymbol;
+    std::vector<t_atom> mArgs;
+};
+
+// This class is a wrapper that allows the parsing to happen correctly
+
+template <class T>
+struct FrameLib_MaxClass_ExprParsed : public FrameLib_MaxClass_Expand<T>
+{
+    FrameLib_MaxClass_ExprParsed(const ExprArgumentParser &parsed) :
+    FrameLib_MaxClass_Expand<T>(parsed.symbol(), parsed.count(), parsed.args(), new FrameLib_MaxProxy()) {}
+};
+
+#endif /* EXPRESSION_OBJECT_PARSING_H */
--- FrameLib_Max_Objects/Expressions/fl.complex.expr~.cpp
+++ FrameLib_Max_Objects/Expressions/fl.complex.expr~.cpp
@@ -2,133 +2,16 @@
 #include "FrameLib_ComplexExpression.h"
 #include "FrameLib_MaxClass.h"
 
-// This object parses it's arguments differently to normal, which is handled by pre-parsing the atoms into a different format
-
-class ComplexArgumentParser
-{
-    
-public:
-    
-    ComplexArgumentParser(t_symbol *s, long argc, t_atom *argv) : mSymbol(s)
-    {
-        if (argc && isStreamSpecifier(argv))
-        {
-            argc--;
-            mArgs.push_back(*argv++);
-        }
-        
-        concatenate(argc, argv);
-        
-        while (argc--)
-        {
-            if (atom_getsym(argv) == gensym("/expr"))
-                concatenate(argc, ++argv);
-            else
-                mArgs.push_back(*argv++);
-        }
-    }
-    
-    t_symbol *symbol() const    { return mSymbol;}
-    long count() const          { return static_cast<long>(mArgs.size());}
-    t_atom* args() const        { return const_cast<t_atom*>(mArgs.data()); }
-
-private:
-    
-    // Stream Specifier Detector
-    
-    bool isStreamSpecifier(t_atom *a)
-    {
-        if (atom_gettype(a) == A_SYM)
-        {
-            t_symbol *sym = atom_getsym(a);
-            return strlen(sym->s_name) > 1 && sym->s_name[0] == '=';
-        }
-        
-        return false;
-    }
-    
-    // Tag Detection
-
-    bool isValidTag(t_atom *a)
-    {
-        t_symbol *sym = atom_getsym(a);
-        size_t len = strlen(sym->s_name);
-        
-        // Attribute
-        
-        if (len > 1 && sym->s_name[0] == '@')
-            return true;
-        
-        // Input tag
-        
-        if (len > 2 && sym->s_name[0] == '[' && sym->s_name[len - 1] == ']')
-            return true;
-        
-        // Basic parameter tag test
-        
-        if (len <= 1 || sym->s_name[0] != '/')
-            return false;
-     
-        // Escape division by known constants
-        
-        if (sym == gensym("/i") || sym == gensym("/pi") || sym == gensym("/epsilon") || sym == gensym("/e") || sym == gensym("/inf"))
-            return false;
-
-        // Require a parameter tag to have only letters after the slash
-        
-        for (const char *c = sym->s_name + 1; *c; c++)
-            if (!((*c >= 'a') && (*c <= 'z')) || ((*c >= 'A') && (*c <= 'Z')))
-                return false;
-        
-        return true;
-    }
-
-    // Concatenator
-    
-    void concatenate(long& argc, t_atom*& argv)
-    {
-        std::string concatenated;
-        
-        for (; argc && !isValidTag(argv); argc--, argv++)
-        {
-            if (atom_gettype(argv) == A_SYM)
-                concatenated += atom_getsym(argv)->s_name;
-            else
-                concatenated += std::to_string(atom_getfloat(argv));
-            
-            // Add whitespace between symbols
-            
-            concatenated += " ";
-        }
-        
-        if (concatenated.length())
-        {
-            t_symbol *arg = gensym(concatenated.c_str());
-            mArgs.push_back(t_atom());
-            atom_setsym(&mArgs.back(), arg);
-        }
-    }
-    
-    t_symbol *mSymbol;
-    std::vector<t_atom> mArgs;
-};
-
-// This class is a wrapper that allows the parsing to happen correctly
-
-struct FrameLib_MaxClass_ComplexExpression_Parsed : public FrameLib_MaxClass_Expand<FrameLib_ComplexExpression>
-{
-    FrameLib_MaxClass_ComplexExpression_Parsed(const ComplexArgumentParser &parsed) :
-    FrameLib_MaxClass(parsed.symbol(), parsed.count(), parsed.args(), new FrameLib_MaxProxy()) {}
-};
+#include "expression_object_parsing.h"
 
 // Max Class (inherits from the parsed version which inherits the standard max class)
 
-struct FrameLib_MaxClass_ComplexExpression : public FrameLib_MaxClass_ComplexExpression_Parsed
+struct FrameLib_MaxClass_ComplexExpression : public FrameLib_MaxClass_ExprParsed<FrameLib_ComplexExpression>
 {
     // Constructor
     
     FrameLib_MaxClass_ComplexExpression(t_symbol *s, long argc, t_atom *argv) :
-        FrameLib_MaxClass_ComplexExpression_Parsed(ComplexArgumentParser(s, argc, argv)) {}
+        FrameLib_MaxClass_ExprParsed<FrameLib_ComplexExpression>(ExprArgumentParser(s, argc, argv, true)) {}
 };
 
 #ifndef FRAMELIB_MAX_SINGLE_OBJECT
