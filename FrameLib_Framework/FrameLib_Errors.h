e5b1b9fcc4fc252711d6aeac982c312a1c5e46a0
Alex Harker <ajharker@gmail.com>
2019-11-24 17:23:15 +0000

    Remove unneeded public keyword.

--- FrameLib_Framework/FrameLib_Errors.h
+++ FrameLib_Framework/FrameLib_Errors.h
@@ -98,9 +98,7 @@ public:
         
         const static int sCharArraySize = 8192;
         const static int sReportArraySize = 1024;
-        
-    public:
-        
+                
     public:
         
         /**
