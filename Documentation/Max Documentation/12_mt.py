2fbc4e89073ac69e9d78a6ab9dc93097688744bc
James Bradbury <jamesbradbury93@gmail.com>
2020-05-28 15:52:51 +0100

    wrap it in a main function

--- Documentation/Max Documentation/12_mt.py
+++ Documentation/Max Documentation/12_mt.py
@@ -16,35 +16,36 @@ def append_tabs(patch, object_name, source):
     for tab in converted['patcher']['boxes']:
         source['patcher']['boxes'].append(tab)
 
+def main():
 # Find binary and ternary objects
-p = Path("../../FrameLib_Max_Objects")
-for z in p.glob("**/*"):
-    object_name = z.stem
-    category = z.parent.stem
-    if object_name[:3] == "fl.":
-        if category == "Binary":
-            binary.append(object_name)
-        if category == "Ternary":
-            ternary.append(object_name)
+    p = Path("../../FrameLib_Max_Objects")
+    for obj in p.glob("**/*"):
+        object_name = obj.stem
+        category = obj.parent.stem
+        if object_name[:3] == "fl.":
+            if category == "Binary":
+                binary.append(object_name)
+            if category == "Ternary":
+                ternary.append(object_name)
 
-p = Path('help_files/templates')
-for object_path in p.glob('**/*'):
-    if object_path.stem in binary or object_path.stem in ternary:
-        template = read_json(object_path)
-        append_tabs(mismatch, object_path.stem, template)
+    # Now insert the necessary tabs 
+    templates = Path('help_files/templates')
+    for object_path in templates.glob('**/*'):
+        if object_path.stem in binary or object_path.stem in ternary:
+            template = read_json(object_path)
+            append_tabs(mismatch, object_path.stem, template)
 
-        if object_path.stem in binary:
-            append_tabs(trigger_ins, object_path.stem, template) 
+            if object_path.stem in binary:
+                append_tabs(trigger_ins, object_path.stem, template) 
 
-        write_json(
-            Path(object_path), 
-            template
-        )
+            write_json(
+                object_path,
+                template
+            )
 
-# Now move all the help files to the project directory
-
-
-
+    # Now collect up and move all the templates to the dist
+    # We could do this in the previous loop, but I think is clearer
+    dest = Path("../../Current Test Version/FrameLib/help")
     
 
 
