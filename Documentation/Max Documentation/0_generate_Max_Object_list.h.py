066584cdb17b46add2c700ce083ac39e5d6778b5
James Bradbury <jamesbradbury93@gmail.com>
2020-09-11 14:23:26 +0100

    refactor 0 script

--- Documentation/Max Documentation/0_generate_Max_Object_list.h.py
+++ Documentation/Max Documentation/0_generate_Max_Object_list.h.py
@@ -6,14 +6,31 @@ from FrameLibDocs.variables import package_root
 from pathlib import Path
 
 
-def write_comma(counter: int, ceiling: int):
+def write_comma(counter: int, ceiling: int) -> None:
     if counter < ceiling - 1:
         op.write(",")
         op.write("\n")
     else:
         op.write("\n")
 
-
+def name_sanitisation(name: str) -> str:
+    name = name.split(">")[0]
+    name = name.split(",")[0]
+    if name == "FrameLib_MaxClass_ToMax":
+        name = "FrameLib_ToHost"
+    elif name == "FrameLib_MaxClass_FromMax":
+        name = "FrameLib_FromHost"
+    elif name == "FrameLib_MaxClass_Info":
+        name = "FrameLib_Info"
+    elif name == "FrameLib_MaxClass_Read":
+        name = "FrameLib_Read"
+    elif name == "FrameLib_MaxClass_ComplexExpression":
+        name = "FrameLib_ComplexExpression"
+    elif name == "FrameLib_MaxClass_Expression":
+        name = "FrameLib_Expression"
+    elif name == "FrameLib_MaxClass_ContextControl":
+        name = "FrameLib_ContextControl"
+    return name
 
 ignored_objects = [x for x in sys.argv[1:]]
 ignored_objects.append("ibuffer_access") # also ignore the ibuffer_access file
@@ -47,42 +64,19 @@ for counter, (category_folder, name) in enumerate(source_file_list):
         # flatten it with no spaces whatsoever
         source_file = cpp.read().replace("\n", "").replace(" ", "")
         search_area = source_file.split('extern"C"intC74_EXPORTmain(void){')[1]
-        # Do this just before info gets cleaved
-        fl_object_name = search_area.split("<")[1]
-        fl_object_name = fl_object_name.split(">")[0]
-        # if it contains ,kAllInputs remove
-        if "," in fl_object_name:
-            fl_object_name = fl_object_name.split(",")[0]
-        # a bit more pruning
-        if fl_object_name == "FrameLib_MaxClass_ToMax":
-            fl_object_name = "FrameLib_ToHost"
-        elif fl_object_name == "FrameLib_MaxClass_FromMax":
-            fl_object_name = "FrameLib_FromHost"
-        elif fl_object_name == "FrameLib_MaxClass_Info":
-            fl_object_name = "FrameLib_Info"
-        elif fl_object_name == "FrameLib_MaxClass_Read":
-            fl_object_name = "FrameLib_Read"
-        elif fl_object_name == "FrameLib_MaxClass_ComplexExpression":
-            fl_object_name = "FrameLib_ComplexExpression"
-        elif fl_object_name == "FrameLib_MaxClass_Expression":
-            fl_object_name = "FrameLib_Expression"
-        elif fl_object_name == "FrameLib_MaxClass_ContextControl":
-            fl_object_name = "FrameLib_ContextControl"
-        ## Now limit search area
+        fl_object_name = name_sanitisation(search_area.split("<")[1])
         search_area = search_area.split("<")[0]
 
         # infer type with brutal checking by looking at text in the extern bit (search area)
         if "_Expand" in search_area:
             op.write(f"FrameLib_Expand<{fl_object_name}>")
             ## Condition to check if its the last one
-            write_comma(counter, len(source_file_list))
         elif "_Expand" not in search_area and "makeClass" in search_area:
             op.write(f"FrameLib_Expand<{fl_object_name}>")
             ## Condition to check if its the last one
-            write_comma(counter, len(source_file_list))
         elif "_Expand" not in search_area and "makeClass" not in search_area:
             op.write(fl_object_name)
-            write_comma(counter, len(source_file_list))
+        write_comma(counter, len(source_file_list))
 
 ## Demarcate end of this section
 op.write('\n\n>;\n\n')
@@ -90,34 +84,11 @@ op.write('\n\n>;\n\n')
 ## Start const bit
 for category_folder, name in source_file_list:
     with open(os.path.join(category_folder, name), "r") as cpp:
-        source_file = (
-            cpp.read().replace("\n", "").replace(" ", "")
-        )  # flatten it with no spaces whatsoever
+        source_file = (cpp.read().replace("\n", "").replace(" ", ""))  # flatten it with no spaces whatsoever
         search_area = source_file.split('extern"C"intC74_EXPORTmain(void){')[1]
 
-        ## Do this just before info gets cleaved
-        fl_object_name = search_area.split("<")[1]
-        fl_object_name = fl_object_name.split(">")[0]
-        ## if it contains ,kAllInputs remove
-        if "," in fl_object_name:
-            fl_object_name = fl_object_name.split(",")[0]
-        ## a bit more pruning
-        if fl_object_name == "FrameLib_MaxClass_ToMax":
-            fl_object_name = "FrameLib_ToHost"
-        elif fl_object_name == "FrameLib_MaxClass_FromMax":
-            fl_object_name = "FrameLib_FromHost"
-        elif fl_object_name == "FrameLib_MaxClass_Info":
-            fl_object_name = "FrameLib_Info"
-        elif fl_object_name == "FrameLib_MaxClass_Read":
-            fl_object_name = "FrameLib_Read"
-        elif fl_object_name == "FrameLib_MaxClass_ComplexExpression":
-            fl_object_name = "FrameLib_ComplexExpression"
-        elif fl_object_name == "FrameLib_MaxClass_Expression":
-            fl_object_name = "FrameLib_Expression"
-        elif fl_object_name == "FrameLib_MaxClass_ContextControl":
-            fl_object_name = "FrameLib_ContextControl"
+        fl_object_name = name_sanitisation(search_area.split("<")[1])
 
-        ## Now limit search area
         search_area = search_area.split("<")[0]
         no_ext = os.path.splitext(name)[0]
         no_ext = no_ext.split("~")[0]
