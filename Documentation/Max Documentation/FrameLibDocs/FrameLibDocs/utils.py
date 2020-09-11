1d7821881886859c140eedf0defc28264940b697
James Bradbury <jamesbradbury93@gmail.com>
2020-09-11 15:34:49 +0100

    refactor utils

--- Documentation/Max Documentation/FrameLibDocs/FrameLibDocs/utils.py
+++ Documentation/Max Documentation/FrameLibDocs/FrameLibDocs/utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import random as rn
 import json
 import yaml
@@ -17,32 +18,10 @@ def strip_extension(input_string: str, max_splits: int) -> str:
     return output
 
 
-def check_make(folder_check: str):
-    """Takes a directory name, checks if it exists and makes."""
-    if not os.path.isdir(folder_check):
-        os.mkdir(folder_check)
-
-
-def remove_ds(list_in: list) -> list:
-    if ".DS_Store" in list_in:
-        list_in.remove(".DS_Store")
-    return list_in
-
-
-def thin_list(list_to_thin: list, things_to_thin: list) -> list:
-    """
-    Takes a list_to_thin and removes each element of things_to_thin from it (if it exists)
-    """
-    for badness in things_to_thin:
-        if badness in list_to_thin:
-            list_to_thin.remove(badness)
-    return list_to_thin
-
-
 def write_json(json_file_path: str, dict_to_write: dict):
     """Takes a dictionary and writes it to JSON file."""
-    path = os.path.dirname(json_file_path)
-    check_make(path)
+    parent = Path(json_file_path).parent
+    parent.mkdir(exist_ok=True)
     with open(json_file_path, "w+") as fp:
         json.dump(dict_to_write, fp, indent=4)
 
@@ -60,18 +39,6 @@ def read_yaml(yaml_file_path: str) -> dict:
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-
-
-def cd_up(path: str, num: int) -> str:
-    """Given path, traverse num directories up from it"""
-    t_path = path
-    for _ in range(num):
-        t_path = os.path.dirname(t_path)
-    return t_path
-
-
-def get_path():
-    return os.path.dirname(os.path.realpath(__file__))
 
 #-- Functions to make the console prints much more pleasant --#
 
