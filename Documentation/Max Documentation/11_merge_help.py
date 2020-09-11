b93662c0dbb526d0cb4ef437361c5c6dbc523d92
James Bradbury <jamesbradbury93@gmail.com>
2020-09-11 16:48:23 +0100

    refactor help scripts

--- Documentation/Max Documentation/11_merge_help.py
+++ Documentation/Max Documentation/11_merge_help.py
@@ -1,39 +1,29 @@
-import os, sys, json
-from FrameLibDocs.utils import read_json, write_json, strip_extension
+from FrameLibDocs.utils import read_json, write_json
+from FrameLibDocs.variables import help_dir, current_version
 
-# TODO
-# Devise a method for merging additional patchers to specific names
-# This can be useful for merging a mismatch/trigger_ins patcher
 
-# the path of this script
-this_script = os.path.dirname(os.path.realpath(__file__))
-templated_files = os.listdir(
-    os.path.join(this_script, 'help_files', 'templates')
-)
-for i in templated_files:
-    if i[:3] != 'fl.':
-        templated_files.remove(i)
-
-internal_tabs = os.listdir(
-    os.path.join(this_script, 'help_files', 'internal_tabs')
-)
 
 def main():
-    for i in templated_files:
-        template = read_json(os.path.join(this_script, 'help_files', 'templates', i))
-        object_name = strip_extension(i, 1)
-        companion_name = f'{object_name}.maxhelp'
-        # now look for a patcher containing tabs
-        if companion_name in internal_tabs:
-            tabs = read_json(os.path.join(this_script, 'help_files', 'internal_tabs', companion_name))
-            tabs_boxes = tabs['patcher']['boxes']
-            for box in tabs_boxes:
-                template['patcher']['boxes'].append(box)
+    template_dir = help_dir / "templates"
+    internal_dir = help_dir / "internal_tabs"
+    external_dir = current_version / "FrameLib" /  "externals"
+    master_template = help_dir / "help_template.maxhelp"
+
+    templates = [x for x in template_dir.rglob("fl.*.maxhelp")]
+    for t in templates:
+        template = read_json(t)
+        tabs = read_json(internal_dir / t.name)
+        tabs_boxes = tabs['patcher']['boxes']
+        for box in tabs_boxes:
+            template['patcher']['boxes'].append(box)
             
             write_json(
-                os.path.join(this_script, 'help_files', 'templates', f'{object_name}.maxhelp'),
+                template_dir / t.name,
                 template, 
             )
+
+if __name__ == "__main__":
+    main()
 
 
     
