e55caeae4015765d4fccdf0649357b3ada7b505d
James Bradbury <jamesbradbury93@gmail.com>
2020-09-11 18:06:53 +0100

    shorten docstring

--- Documentation/Max Documentation/1_create_category_database.py
+++ Documentation/Max Documentation/1_create_category_database.py
@@ -4,15 +4,15 @@ from FrameLibDocs.utils import write_json
 
 def main():
     """
-    This creates a category database in .json format.
-    This is used by edit_raw_XML.py to assign object categories to the xml files.
+    Creates a category database in .json format.
+    Used by edit_raw_XML.py to assign object categories to the xml files.
     """
 
     max_objects = package_root / "FrameLib_Max_Objects"
     d = {}
 
     for f in max_objects.rglob("fl.*.cpp"):
-        category = f"FrameLib {f.parent.parts[-1]}"
+        category = f'FrameLib {str(f.parent.parts[-1])}'
         if category in d:
             d[category].append(f.stem)
         else:
