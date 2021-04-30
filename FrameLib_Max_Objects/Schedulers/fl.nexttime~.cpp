6d8c34c4ef0735c0df6c758128297c02fa1489e7
Alex Harker <ajharker@gmail.com>
2021-04-30 11:24:14 +0100

    Compile next time object separately to the scheduler

--- FrameLib_Max_Objects/Schedulers/fl.nexttime~.cpp
+++ FrameLib_Max_Objects/Schedulers/fl.nexttime~.cpp
@@ -4,5 +4,5 @@
 
 extern "C" int C74_EXPORT main(void)
 {
-    FrameLib_MaxClass_Expand<FrameLib_Recursive>::makeClass("fl.recursive~");
+    FrameLib_MaxClass_Expand<FrameLib_Recursive::Time>::makeClass("fl.nexttime~");
 }
