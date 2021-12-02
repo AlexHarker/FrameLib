798bb8307331ca3d311084b0760dc3370b63d50e
Alex Harker <ajharker@gmail.com>
2021-12-02 17:50:40 +0000

    Move to using HISSTools_Library random number generator

--- FrameLib_Objects/Common_Utilities/FrameLib_RandGen.h
+++ FrameLib_Objects/Common_Utilities/FrameLib_RandGen.h
@@ -2,64 +2,38 @@
 #ifndef _FRAMELIB_RANDGEN_
 #define _FRAMELIB_RANDGEN_
 
-#include "FrameLib_Types.h"
+//#include "FrameLib_Types.h"
 
-class FrameLib_RandGen
+#include "RandomGenerator.hpp"
+
+class FrameLib_RandGen : private random_generator<>
 {
-    static constexpr uint32_t cmwc_lag_size = 32;
-    static constexpr uint64_t cmwc_a_value = 987655670LL;
-    
 public:
-    
-    FrameLib_RandGen()              { randSeed(); }
-    
+        
     // Seeding (specific / random values)
     
-    void initSeed(uint32_t *init)   { initSeedCMWC(init); }
-    void randSeed()                 { randSeedCMWC(); }
+    void initSeed(uint32_t *init)   { random_generator<>::seed(init); }
+    void randSeed()                 { random_generator<>::rand_seed(); }
     
     // Generate a Single Pseudo-random Unsigned Integer (full range /  in the range [0, n] / in the range [lo, hi])
     
-    uint32_t randInt();
-    uint32_t randInt(uint32_t n);
-    int32_t randInt(int32_t lo, int32_t hi);
+    uint32_t randInt()                              { return random_generator<>::rand_int(); }
+    uint32_t randInt(uint32_t n)                    { return random_generator<>::rand_int(n); }
+    int32_t randInt(int32_t lo, int32_t hi)         { return random_generator<>::rand_int(lo, hi); }
     
     // Generate a 32 bit Random Double (in the range [0,1] / in the range [0, n] / in the range [lo, hi])
     
-    double randDouble();
-    double randDouble(double n);
-    double randDouble(double lo, double hi);
+    double randDouble()                             { return random_generator<>::rand_double(); }
+    double randDouble(double n)                     { return random_generator<>::rand_double(n); }
+    double randDouble(double lo, double hi)         { return random_generator<>::rand_double(lo, hi); }
 
     // Generate a 32 bit Random Double of Gaussian Distribution with given Mean / Deviation
     
-    double randGaussian(double mean, double dev);
+    double randGaussian(double mean, double dev)    { return random_generator<>::rand_gaussian(mean, dev); }
     
     // Generate two independent gaussians (Mean 0 and Deviation 1)
     
-    void randGaussians(double& x, double& y);
-
-private:
-
-    // Methods Specific to the RNG Algorithm
-    
-    // Basic Generator
-
-    inline uint32_t CMWC();
-    
-    // Seeding (specific / OS-specific random values)
-    
-    void initSeedCMWC(uint32_t *init);    
-    void randSeedCMWC();
-    
-    // Gaussian Helper
-    
-    void randGaussians(double& x, double& y, double& R);
-
-    // State
-    
-    uint32_t mIncrement;
-    uint32_t mCarry;
-    uint32_t mState[cmwc_lag_size];
+    void randGaussians(double& x, double& y)         { return random_generator<>::rand_gaussians(x, y); }
 };
 
 #endif
