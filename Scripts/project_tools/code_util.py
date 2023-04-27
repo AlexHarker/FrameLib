5a9685409f26f7f8d7af709b22aa294624919558
Alex Harker <ajharker@gmail.com>
2023-04-27 17:45:13 +0100

    Clean-up

--- Scripts/project_tools/code_util.py
+++ Scripts/project_tools/code_util.py
@@ -1,11 +1,14 @@
-from . import file_util
+
+from . file_util import rw_file
 from . object_info import fl_object
 from . path_util import fl_paths
 
+from . import file_util
 
-# Get a list of items from a file with the specified bounds based on a regex
 
-def item_list(path: str, exp: str, bounds: list):
+# Get a list of items from a string with the specified bounds based on a regex
+
+def item_list(data: str, exp: str, bounds: list):
     
     import re
     
@@ -15,35 +18,34 @@ def item_list(path: str, exp: str, bounds: list):
     s2 = False
     started = False
     
-    with open(path) as f:
-        for line in f:
-            match = regex.search(line)
+    for line in data.splitlines():
+        match = regex.search(line)
             
-            if bounds[0] in line:
-                s1 = True
-            if bounds[2] in line:
-                s2 = True
+        if bounds[0] in line:
+            s1 = True
+        if bounds[2] in line:
+            s2 = True
               
-            if started and (bounds[1] in line or bounds[3] in line):
-                return list
+        if started and (bounds[1] in line or bounds[3] in line):
+            return list
                 
-            started = s1 and s2
+        started = s1 and s2
             
-            if started and match is not None:
-                list.append([match.group(1), match.group(0)])
+        if started and match is not None:
+            list.append([match.group(1), match.group(0)])
 
     return list
 
 
-# Insert code into a file between
+# Insert code into a string based on the given bounds and regex for finding items
 
-def insert_code(path: str, contents: str, class_name: str, info: fl_object, bounds: list, exp: str, layout: str = ""):
+def insert_code(data: str, contents: str, class_name: str, info: fl_object, bounds: list, exp: str, layout: str = ""):
 
     # Core variables
     
     category = info["category"]
     category_compare = category.replace("_", " ")
-    categories = item_list(path, "^.*// (.*)", [bounds[0], bounds[1], bounds[0], bounds[1]])
+    categories = item_list(data, "^.*// (.*)", [bounds[0], bounds[1], bounds[0], bounds[1]])
     
     # Loop over existent categories
     
@@ -55,19 +57,17 @@ def insert_code(path: str, contents: str, class_name: str, info: fl_object, boun
         
             # Get a list of objects
             
-            objects = item_list(path, exp, [bounds[0], bounds[1]] + ["// " + category_compare, "//"])
+            objects = item_list(data, exp, [bounds[0], bounds[1]] + ["// " + category_compare, "//"])
             
             # If the item should precede an existing one insert it there
             
             for o in objects:
                 if o[0] > class_name:
-                    file_util.insert_file(path, contents, [bounds[0], o[1]])
-                    return
+                    return file_util.insert(data, contents, [bounds[0], o[1]])
             
             # Otherwise insert at the final position
 
-            file_util.insert_file(path, contents, [bounds[0], categories[index][1]], True)
-            return
+            return file_util.insert(data, contents, [bounds[0], categories[index][1]], True)
             
     # If the category doesn't exist it will require creating
     
@@ -77,18 +77,19 @@ def insert_code(path: str, contents: str, class_name: str, info: fl_object, boun
     
     for c in categories:
         if c[0] > category_compare:
-            file_util.insert_file(path, contents, [bounds[0], layout + "// " + c[0]])
-            return
-    
+            return file_util.insert(data, contents, [bounds[0], layout + "// " + c[0]])
+            
     # Otherwise insert at the final position
     
-    file_util.insert_file(path, contents, [bounds[0], bounds[2]])
+    return file_util.insert(data, contents, [bounds[0], bounds[2]])
     
     
 # Insert code into the cpp file for a single build version of framelib
     
 def insert_single_build(path: str, template: str, class_name: str, info: fl_object, bounds: list):
    
+    file = rw_file(path)
+    
     contents = file_util.templated_string(fl_paths().code_template(template), info)
     exp = "^.*?(fl.*~).*"
     insert_code(path, contents, class_name, info, bounds + ["    // Unary Operators"], exp, "    ")
@@ -96,9 +97,8 @@ def insert_single_build(path: str, template: str, class_name: str, info: fl_obje
     
 # Insert code into the header file that lists all framelib objects
 
-def insert_object_list_include(info: fl_object):
+def update_object_list_include(info: fl_object, warn: bool):
 
-    path = fl_paths().objects_export_header()
     contents = file_util.templated_string(fl_paths().code_template("object_list_include"), info)
     exp = "^.*/FrameLib_Objects/" + info["category"] + "/(.*)\.h.*"
     insert_code(path, contents, info["object_class"], info, ["#ifndef", "#endif", "// Operators"], exp)
