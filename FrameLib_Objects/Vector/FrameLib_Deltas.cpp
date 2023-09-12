8f59946d45f9dec004156e98a386edc50754988f
fearne <gitfearne@proton.me>
2023-09-12 12:52:55 +0100

    format info

--- FrameLib_Objects/Vector/FrameLib_Deltas.cpp
+++ FrameLib_Objects/Vector/FrameLib_Deltas.cpp
@@ -33,8 +33,7 @@ FrameLib_Deltas::FrameLib_Deltas(FrameLib_Context context, const FrameLib_Parame
 std::string FrameLib_Deltas::objectInfo(bool verbose)
 {
     return formatInfo("Calculates the differences between adjacent values within the frame: "
-                      "The direction of the comparison is set by the compare parameter."
-                      "The way the initial element is calculated is set by the initial parameter.",
+                      "Edge element behaviour can be changed with a combination of the mode and padding parameters."
                       "Calculates the differences between adjacent values within the frame.", verbose);
 }
 
